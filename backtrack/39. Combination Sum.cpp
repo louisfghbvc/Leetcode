99610c31e1d32dc220092d8da74ca37586fd469c
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-02-17 10:03:51 +0800

    Update 39. Combination Sum.cpp

--- backtrack/39. Combination Sum.cpp
+++ backtrack/39. Combination Sum.cpp
@@ -1,13 +1,12 @@
+// 12ms 10.8MB
 class Solution {
 public:
     void dfs(int st, int &t, int v, vector<int> tmp, vector<vector<int>> &res, vector<int>& arr){
-        if(v > t)
-            return;
         if(v == t){
             res.push_back(tmp);
             return;
         }
-        for(int i = st; i < arr.size(); ++i){
+        for(int i = st; i < arr.size() && v + arr[i] <= t; ++i){
             tmp.push_back(arr[i]);
             dfs(i, t, v + arr[i], tmp, res, arr);
             tmp.pop_back();
@@ -16,7 +15,7 @@ public:
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> res;
-        
+        sort(candidates.begin(), candidates.end());
         dfs(0, target, 0, {}, res, candidates);
         
         return res;
