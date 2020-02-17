b4330815b6c387f0afa05a0219156f306a167fa0
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-02-17 11:01:55 +0800

    Update 46. Permutations.cpp

--- backtrack/46. Permutations.cpp
+++ backtrack/46. Permutations.cpp
@@ -1,3 +1,24 @@
+//8ms 9.2MB
+class Solution {
+public:
+    void dfs(int st, vector<vector<int>> &ans, vector<int> &arr){
+        if(st == arr.size()){
+            ans.push_back(arr);
+            return;
+        }
+        for(int i = st; i < arr.size(); ++i){
+            swap(arr[st], arr[i]);
+            dfs(st + 1, ans, arr);
+            swap(arr[st], arr[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        dfs(0, ans, nums);
+        return ans;
+    }
+};
+/**12ms 9.9MB
 class Solution {
 public:
     void dfs(int d, vector<int> v, vector<vector<int>> &ans, vector<bool> &vis, vector<int> &arr){
@@ -21,3 +42,4 @@ public:
         return ans;
     }
 };
+**/
