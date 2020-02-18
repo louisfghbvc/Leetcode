de7220829103a01b98d21dbb75118e681faf6981
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-02-18 16:39:21 +0800

    Create 357. Count Numbers with Unique Digits.cpp

---
+++ backtrack/357. Count Numbers with Unique Digits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+	void dfs(int d, int &n, int &used, int &res){
+		if(d == n){
+			return;
+		}
+		for(int i = (d == 0) ? 1 : 0; i <= 9; ++i){
+			if(used & (1<<i)) continue;
+			res++;
+            used |= (1<<i);
+			dfs(d + 1, n, used, res);
+            used ^= (1<<i);
+		}
+	}
+    int countNumbersWithUniqueDigits(int n) {
+        int cnt = 0, used = 0;
+        dfs(0, n, used, cnt);
+        return cnt + 1;
+    }
+};
