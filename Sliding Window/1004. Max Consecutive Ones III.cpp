a45be49e229d89623614f3b41ba85918ebad0a48
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-02-24 15:19:57 +0800

    Update 1004. Max Consecutive Ones III.cpp

--- Sliding Window/1004. Max Consecutive Ones III.cpp
+++ Sliding Window/1004. Max Consecutive Ones III.cpp
@@ -1,11 +1,11 @@
-//60ms 13.8MB
+//52~64ms 13.8MB
 class Solution {
 public:
     int longestOnes(vector<int>& A, int K) {
         int l = 0, r, mx = 0;
         for(r = 0; r < A.size(); ++r){
             if(!A[r]) K--;
-            while(K < 0 && !A[l++]) K++;
+            if(K < 0 && !A[l++]) K++;
             mx = max(r - l + 1, mx);
         }
         return mx;
