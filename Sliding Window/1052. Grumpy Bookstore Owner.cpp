cb3f1e044fdb6b1f4dce45c3db1a76564d64c035
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-02-24 16:41:22 +0800

    Update 1052. Grumpy Bookstore Owner.cpp

--- Sliding Window/1052. Grumpy Bookstore Owner.cpp
+++ Sliding Window/1052. Grumpy Bookstore Owner.cpp
@@ -1,19 +1,15 @@
-//40ms 11.5MB
+//32ms 11.4MB
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int X) {
-        int sum = 0, n = customers.size(), mx = 0;
-        for(int i = 0; i < n; ++i){
-            if(!grumpy[i]) 
-                sum += customers[i];
-        }
-        
-        int l = 0, tmp = 0;
+        int sum = 0, n = customers.size(), mx = 0, tmp = 0, l = 0;
         for(int r = 0; r < n; ++r){
-            if(grumpy[r])
+            if(!grumpy[r]) 
+                sum += customers[r];
+            else
                 tmp += customers[r];
             if(r - l + 1 > X && grumpy[l++]){
-               tmp -= customers[l-1];
+                tmp -= customers[l-1];
             }
             mx = max(mx, tmp);
         }
