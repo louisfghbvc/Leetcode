4a23817915c0e5ec1722075919a4b80152c1f9c9
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-02-25 15:21:27 +0800

    Create 1108. Defanging an IP Address.cpp

---
+++ String/1108. Defanging an IP Address.cpp
@@ -0,0 +1,14 @@
+//0ms 8.3MB
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string res = "";
+        for(int i = 0; i < address.size(); ++i){
+            if(address[i] == '.')
+                res += "[.]";
+            else
+                res += address[i];
+        }
+        return res;
+    }
+};
