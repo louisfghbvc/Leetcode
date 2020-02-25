f1f621e30293e97e7686b04a93bc0f1362d27a1a
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-02-25 15:07:37 +0800

    Update 239. [Like and Useful]Sliding Window Maximum.cpp

--- Sliding Window/239. [Like and Useful]Sliding Window Maximum.cpp
+++ Sliding Window/239. [Like and Useful]Sliding Window Maximum.cpp
@@ -3,12 +3,12 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> ans;
-		vector<int> dq;
+	vector<int> dq;
         for(int i = 0; i < nums.size(); ++i){
             if(!dq.empty() && dq.front() == i - k) dq.erase(dq.begin());
             while(!dq.empty() && nums[dq.back()] < nums[i]) dq.pop_back();
             dq.push_back(i);
-        	if(i >= k - 1)
+            if(i >= k - 1)
                 ans.push_back(nums[dq.front()]);
         }
         return ans;
