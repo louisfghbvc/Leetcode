5ed386ec543927627809b2b76c30cdcaf203c7d0
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-03-02 17:39:27 +0800

    Create 876. [Like]Middle of the Linked List.cpp

---
+++ Linked List/876. [Like]Middle of the Linked List.cpp
@@ -0,0 +1,21 @@
+//0ms 7.6MB Fast, Slow algorithm, Cool
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
