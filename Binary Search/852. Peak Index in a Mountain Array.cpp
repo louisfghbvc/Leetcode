a23093f6037110f18520e90fbe7148f70fec0ba9
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-03-04 00:23:10 +0800

    Create 852. Peak Index in a Mountain Array.cpp

---
+++ Binary Search/852. Peak Index in a Mountain Array.cpp
@@ -0,0 +1,15 @@
+//12ms - 16ms 8.6MB
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& A) {
+        int l = 0, r = A.size() - 1, mid;
+        while(l <= r){
+            mid = (l + r) / 2;
+            if(A[mid] < A[mid + 1])
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return l;
+    }
+};
