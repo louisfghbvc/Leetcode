1f3cfaad9819b1f338be637c7a7f8eaed46acd18
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-03-06 16:54:10 +0800

    Update 1237. Find Positive Integer Solution for a Given Equation.cpp

--- Binary Search/1237. Find Positive Integer Solution for a Given Equation.cpp
+++ Binary Search/1237. Find Positive Integer Solution for a Given Equation.cpp
@@ -9,6 +9,23 @@
  *     int f(int x, int y);
  * };
  */
+
+//0ms 8.0MB O(X + Y), Impressive Lee
+class Solution {
+public:
+    vector<vector<int>> findSolution(CustomFunction& customfunction, int z) {
+        vector<vector<int>> res;
+        int y = 1000;
+        for(int x = 1; x <= 1000; ++x){
+            while(y > 1 && customfunction.f(x, y) > z) y--;
+            if(customfunction.f(x, y) == z)
+                res.push_back({x, y});
+        }
+
+        return res;
+    }
+};
+
 //4ms 7.7MB, Binary Search 
 class Solution {
 public:
