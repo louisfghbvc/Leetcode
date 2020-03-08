b8e52bdc3665b04d7d3532ce2fcaa7f9ebc879ac
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-03-08 16:39:41 +0800

    Simple

---
+++ String/1374. Generate a String With Characters That Have Odd Counts.cpp
@@ -0,0 +1,17 @@
+//0ms 7.9MB
+class Solution {
+public:
+    string generateTheString(int n) {
+        string ans = "";
+        if(n % 2){
+            for(int i = 0; i < n; ++i)
+                ans += "a";
+        }
+        else{
+            for(int i = 0; i < n - 1; ++i)
+                ans += "a";
+            ans += "b";
+        }
+        return ans;
+    }
+};
