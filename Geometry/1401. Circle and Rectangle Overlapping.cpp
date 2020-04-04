6195b1d0107bc2b5a00e55a35bd58b11f6a33aee
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-04-05 01:05:53 +0800

    lambda, Math

---
+++ Geometry/1401. Circle and Rectangle Overlapping.cpp
@@ -0,0 +1,16 @@
+//circle in rectangle or rectangle in circle. O(N) 
+//0ms 6.1MB
+class Solution {
+public:
+    bool checkOverlap(int r, int xc, int yc, int x1, int y1, int x2, int y2) {
+        auto inside = [&](int x, int y){
+            return abs(x - xc)*abs(x - xc) + abs(y - yc)*abs(y - yc) <= r*r;
+        };
+        if(x1 <= xc && y1 <= yc && x2 >= xc && y2 >= yc) return 1;
+        for(int x = x1; x <= x2; ++x)
+            if(inside(x, y1) || inside(x, y2)) return 1;
+        for(int y = y1; y <= y2; ++y)
+            if(inside(x1, y) || inside(x2, y)) return 1;
+        return 0;
+    }
+};
