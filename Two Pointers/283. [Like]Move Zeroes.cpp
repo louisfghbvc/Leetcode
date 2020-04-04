ce6f3713c253e73c8e95e7885790e07ff13edffc
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-04-04 15:42:52 +0800

    Swap

--- Two Pointers/283. [Like]Move Zeroes.cpp
+++ Two Pointers/283. [Like]Move Zeroes.cpp
@@ -1,9 +1,11 @@
+//O(N)
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         for(int j = 0, i = 0; j < nums.size(); ++j){
-            while(i < nums.size() && nums[i]) i++;
-            if(i < nums.size() && i < j) swap(nums[i], nums[j]);
+            if(nums[j]){
+                swap(nums[i++], nums[j]);   
+            }
         }
     }
 };
