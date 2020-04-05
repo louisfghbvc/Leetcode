a56374e1cb474f6deaea176739bca322197fbb83
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-04-05 14:29:10 +0800

    String. Recursive. Tricky

--- Greedy/1405. [Like And Useful]Longest Happy String.cpp
+++ Greedy/1405. [Like And Useful]Longest Happy String.cpp
@@ -6,8 +6,6 @@ public:
     string longestDiverseString(int a, int b, int c, char aa = 'a', char bb = 'b', char cc = 'c') {
         if(a < b)
             return longestDiverseString(b, a, c, bb, aa, cc);
-        if(a < c)
-            return longestDiverseString(c, b, a, cc, bb, aa);
         if(b < c)
             return longestDiverseString(a, c, b, aa, cc, bb);
         if(b == 0)
