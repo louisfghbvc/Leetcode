fd14f7f1a791794484b7d1b8a6974212e9e030bd
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-04-20 17:57:58 +0800

    Trick.

--- Binary Search/33. [Like]Search in Rotated Sorted Array.cpp
+++ Binary Search/33. [Like]Search in Rotated Sorted Array.cpp
@@ -1,6 +1,36 @@
-//
+//Prefer like this, just 2 case. left part or right part
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while(l <= r){
+            int mid = (l + r) / 2;
+            if(nums[mid] == target) return mid;
+            
+            // left part increasing
+            //must use "<=" make sure target is left part
+            if(nums[l] <= nums[mid]){
+                if(nums[l] <= target && target < nums[mid]){
+                    r = mid - 1;
+                }
+                else{
+                    l = mid + 1;
+                }
+            }
+            // right part increasing
+            else{
+                if(nums[r] >= target && target > nums[mid]){
+                    l = mid + 1;
+                }
+                else{
+                    r = mid - 1;
+                }
+            }
 
-
+        }
+        return -1;
+    }
+};
 
 //Very trick. hard to understand
 class Solution {
