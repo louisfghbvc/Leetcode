59912e78446f5d5c97f5ecd0ebc8c10e9823dbee
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-04-20 16:46:54 +0800

    O(N)->Hard to understand, O(NlogN)

--- Tree/1008. [Like And Useful]Construct Binary Search Tree from Preorder Traversal.cpp
+++ Tree/1008. [Like And Useful]Construct Binary Search Tree from Preorder Traversal.cpp
@@ -1,7 +1,24 @@
-//O(N). Trick
-
-
-
+//O(N). Trick, Hard to Think.
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int id = 0;
+    TreeNode* bstFromPreorder(vector<int>& preorder, int bound = INT_MAX) {
+        if(id >= preorder.size() || preorder[id] > bound) return NULL;
+        TreeNode *root = new TreeNode(preorder[id++]);
+        root->left = bstFromPreorder(preorder, root->val);
+        root->right = bstFromPreorder(preorder, bound);
+        return root;
+    }
+};
 
 //O(N log N), Simple and important
 /**
