95ac47b2b49b3700d68a8b745285ef7d532bab8f
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-04-21 23:17:06 +0800

    Optimal, Binary Search

---
+++ Greedy/[Like]Leftmost Column with at Least a One.cpp
@@ -0,0 +1,28 @@
+//O(NlogM), Binary Search
+each n, find m is one, record min.
+
+//Try to go to left down. O(M+N)
+/**
+ * // This is the BinaryMatrix's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class BinaryMatrix {
+ *   public:
+ *     int get(int x, int y);
+ *     vector<int> dimensions();
+ * };
+ */
+
+class Solution {
+public:
+    int leftMostColumnWithOne(BinaryMatrix &binaryMatrix) {
+        int n = binaryMatrix.dimensions()[0], m = binaryMatrix.dimensions()[1];
+        int ans = m - 1;
+        for(int i = 0; i < n; ++i){
+            while(ans >= 0 && binaryMatrix.get(i, ans) == 1){
+                ans--;
+            }
+        }
+        
+        return ans == m - 1 ? -1 : ans+1;
+    }
+};
