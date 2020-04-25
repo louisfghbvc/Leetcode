eebaed84d9d208ee049b18c367b7da6e198a8e2d
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-04-25 17:25:11 +0800

    Greedy. Lambda, Dp

--- Greedy/55. [Like And Useful]Jump Game.cpp
+++ Greedy/55. [Like And Useful]Jump Game.cpp
@@ -22,6 +22,29 @@ public:
     }
 };
 
+// forward 4ms 7.7MB
+// very greedy. I think the same idea, but don't know how to implement.
+// from st, try get max step.
+//why front index doesn't need to jump? because u step from step,
+//u won't jump to back then jump to begin
+static int p = [](){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    return 0;
+}();
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int x = 0;
+        for(int i = 0; i <= x; ++i){
+            x = max(x, i+nums[i]);
+            if(x >= n-1) return 1;
+        }
+        return 0;
+    }
+};
+
 // dp. bottom-up, from back, 784ms 8.1MB
 static int p = [](){
     ios::sync_with_stdio(0);
