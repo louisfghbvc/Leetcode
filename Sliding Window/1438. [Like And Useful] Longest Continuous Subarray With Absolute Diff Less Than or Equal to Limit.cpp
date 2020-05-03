1c2822245610b38bbbbe191f50aaa603922d18ab
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-05-03 12:50:32 +0800

    Multiset or Deque. Classic Sliding Window.

--- Sliding Window/1438. [Like And Useful] Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp
+++ Sliding Window/1438. [Like And Useful] Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp
@@ -16,4 +16,31 @@ public:
     }
 };
 
-// Solution2. Deque
+// Solution2. Deque. O(N)
+// if u know multiset solution. this is same idea.
+// just maintain min queue, max queue.
+// front == nums[i]. pop_front.
+// impressive. QQ
+// 120ms. 32.8MB
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+        int i = 0, ans = 1;
+        deque<int> maxd, mind;
+        for(int j = 0; j < nums.size(); ++j){
+            while(!maxd.empty() && maxd.back() < nums[j]) maxd.pop_back();
+            while(!mind.empty() && mind.back() > nums[j]) mind.pop_back();
+            maxd.push_back(nums[j]);
+            mind.push_back(nums[j]);
+            
+            if(maxd.front() - mind.front() > limit){
+                if(maxd.front() == nums[i]) maxd.pop_front();
+                if(mind.front() == nums[i]) mind.pop_front();
+                i++;
+            }
+            
+            ans = max(ans, j-i+1);
+        }
+        return ans;
+    }
+};
