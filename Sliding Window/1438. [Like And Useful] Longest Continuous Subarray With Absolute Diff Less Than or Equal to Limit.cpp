ea9a60f753adf018b0c06914c7479b3cf99b2145
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-05-03 12:24:03 +0800

    Multiset or Deque

---
+++ Sliding Window/1438. [Like And Useful] Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp
@@ -0,0 +1,19 @@
+// Solution1. Multiset. O(NlogN)
+// Use one params. to record now_index. maintain bag (max-min) <= limit
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+        int ans = 1;
+        multiset<int> bag;
+        for(int i = 0, j = 0; i < nums.size(); ++i){
+            bag.insert(nums[i]);
+            while(*--bag.end() - *bag.begin() > limit){
+                bag.erase(bag.find(nums[j++]));
+            }
+            ans = max(ans, i-j+1);
+        }
+        return ans;
+    }
+};
+
+// Solution2. Deque
