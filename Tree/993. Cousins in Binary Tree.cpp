592a10eb4c05c375616a5fe71837fcad3c905676
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-05-07 21:29:30 +0800

    DFS, BFS, O(N)

--- Tree/993. Cousins in Binary Tree.cpp
+++ Tree/993. Cousins in Binary Tree.cpp
@@ -30,4 +30,41 @@ public:
     }
 };
 
-// Solution2 bfs
+// Solution2 bfs, each node check if child is x and y. same level x and y exist return true
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isCousins(TreeNode* root, int x, int y) {
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            bool x_exist = 0, y_exist = 0;
+            for(int i = 0; i < size; ++i){
+                TreeNode *cur = q.front(); q.pop();
+                if(cur->val == x) x_exist = true;
+                if(cur->val == y) y_exist = true;
+                if(cur->left && cur->right){
+                    if(cur->left->val == x && cur->right->val == y) return 0;
+                    if(cur->left->val == y && cur->right->val == x) return 0;
+                }
+                if(cur->left)
+                    q.push(cur->left);
+                if(cur->right)
+                    q.push(cur->right);
+            }
+            if(x_exist && y_exist) return 1;
+        }
+        return 0;
+    }
+};
