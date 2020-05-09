50f476ad07f6ace7b6cf95a4e3015fabddc57dbe
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-05-09 16:53:52 +0800

    Math. Trick

---
+++ Math/367. [Like] Valid Perfect Square.cpp
@@ -0,0 +1,18 @@
+// cool solution.
+// O(sqrt(N)). Observe.
+// square 1 = 1
+// 4 = 1 + 3
+// 9 = 1 + 3 + 5
+// 16 = 1 + 3 + 5 + 7
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int i = 1;
+        while(num > 0){
+            num -= i;
+            i += 2;
+        }
+        return num == 0;
+    }
+};
