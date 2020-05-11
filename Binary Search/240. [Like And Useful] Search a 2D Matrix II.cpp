ba215ab3015632123e7f2fcab3bff2ca7fd2f2db
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-05-11 23:49:57 +0800

    Binary Search O(mlogn), Optimal. O(m+n)

--- Binary Search/240. [Like And Useful] Search a 2D Matrix II.cpp
+++ Binary Search/240. [Like And Useful] Search a 2D Matrix II.cpp
@@ -45,3 +45,20 @@ public:
         return 0;
     }
 };
+
+// this is timelimit. auto is bad. slow. because it is big.
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for(auto row : matrix){
+            int l = 0, r = row.size()-1;
+            while(l <= r){
+                int mid = l + (r-l)/2;
+                if(row[mid] == target) return 1;
+                else if(row[mid] < target) l = mid+1;
+                else r = mid-1;
+            }
+        }
+        return 0;
+    }
+};
