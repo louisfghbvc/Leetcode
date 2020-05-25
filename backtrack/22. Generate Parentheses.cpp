c0303b5445a7838091c49b56c9eff2d8631b4d7f
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-05-25 22:04:36 +0800

    Dp, backtraking, Observe

--- backtrack/22. Generate Parentheses.cpp
+++ backtrack/22. Generate Parentheses.cpp
@@ -1,19 +1,40 @@
+// Solution1 simple backtracking.
 class Solution {
 public:
-    void dfs(string s, int l, int r, vector<string> &res){
-        if(l == 0 && r == 0){
-            res.push_back(s);
+    vector<string> ans;
+    void dfs(int c, string s, int n, int i){
+        if(i == 2*n){
+            if(c == 0) ans.push_back(s); 
             return;
-        }
-        if(r > 0)
-            dfs(s + ')', l, r - 1, res);
-        if(l > 0)
-            dfs(s + '(', l - 1, r + 1, res);
-        
+        } 
+        if(c < 0) dfs(c+1, s+")", n, i+1);
+        dfs(c-1, s+"(", n, i+1);
     }
     vector<string> generateParenthesis(int n) {
+        ans.clear();
+        dfs(0, "", n, 0);
+        return ans;
+    }
+};
+
+// Solution2 dp + backtracking, very cool
+// f(0) = ""
+// f(1) = (f(0))
+// f(2) = (f(0))f(1) or (f(1))
+// f(3) = (f(0))f(2) ot (f(2))
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        dfs("", n, 0, ans);
+        if(n == 0){
+            ans.push_back("");
+        }
+        else{
+            for(int c = 0; c < n; ++c)
+                for(const auto& left: generateParenthesis(c))
+                    for(const auto& right: generateParenthesis(n - c - 1))
+                        ans.push_back("(" + left + ")" + right);
+        }
         return ans;
     }
 };
