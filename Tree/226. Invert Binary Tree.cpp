7bfc6d41d92793cff0e794378b34b6b337c279c5
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-06-01 15:27:26 +0800

    Recursive

---
+++ Tree/226. Invert Binary Tree.cpp
@@ -0,0 +1,24 @@
+// swap can reference if is pointer.
+// then recursive. O(logN)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root) return 0;
+        swap(root->left, root->right);
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
