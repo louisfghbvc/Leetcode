cf11883d216d2cc2fe4306e8addf13b4a70628df
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-06-03 14:43:54 +0800

    Hard think, math permutation.

--- backtrack/1467. [Like] Probability of a Two Boxes Having The Same Number of Distinct Balls.cpp
+++ backtrack/1467. [Like] Probability of a Two Boxes Having The Same Number of Distinct Balls.cpp
@@ -1,6 +1,7 @@
 // Very hard to think backtracking.
-// use left means left box remain. if 0 means can calculate combination
-// O(?). 80ms.
+// cm1 is permutation. col is calculate different. cnt is left.
+// and when a new ball is add, divide the ball because duplicate
+// O(6^8). 28ms.
 
 class Solution {
 public:
@@ -9,31 +10,28 @@ public:
     double fac(int x){
         return x < 3 ? x : x*fac(x-1);
     }
-    void dfs(vector<int>& balls, int p, int b1, int b2, int left, double cm1, double cm2){
-        if(!left){
-            for(int i = p; i < balls.size(); ++i){
-                cm2 /= fact[balls[i]];
-                b2 |= 1<<i;
-            }
-            total += cm1*cm2;
-            if(__builtin_popcount(b1) == __builtin_popcount(b2))
-                match += cm1*cm2;
+    void dfs(vector<int>& balls, int p, int col1, int col2, int cnt1, int cnt2, double cm1, double cm2){
+        if(!cnt1 && !cnt2){
+            total += cm1 * cm2;
+            match += (col1==col2) * cm1 * cm2;
         }
-        else if(left > 0 && p < balls.size()){
-            for(int c = 0; c <= balls[p]; ++c){
+        else if(cnt1 >= 0 && cnt2 >= 0){
+            for(int b1 = 0, b2 = balls[p]; b2 >= 0; b1++, b2--){
                 dfs(balls, p+1, 
-                    b1 | (c > 0 ? 1<<p : 0),
-                    b2 | (c < balls[p] ? 1<<p : 0),
-                    left - c,
-                    cm1 / fact[c], 
-                    cm2 / fact[balls[p]-c]
+                    col1 + (b1>0),
+                    col2 + (b2>0),
+                    cnt1 - b1,
+                    cnt2 - b2,
+                    cm1 / fact[b1],
+                    cm2 / fact[b2]
                    );
             }
         }
+        
     }
     double getProbability(vector<int>& balls) {
         int n = accumulate(balls.begin(), balls.end(), 0);
-        dfs(balls, 0, 0, 0, n/2, fac(n/2), fac(n/2));
+        dfs(balls, 0, 0, 0, n/2, n/2, fac(n/2), fac(n/2));
         return match/total;
     }
 };
