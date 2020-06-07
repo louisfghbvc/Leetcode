50ead6d4ba288df794de5cf84b7ee1b1359d3527
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-06-07 13:46:56 +0800

    Sort, Two Pointer, Quick Select and maintain Queue

--- Sort/1471. The k Strongest Values in an Array.cpp
+++ Sort/1471. The k Strongest Values in an Array.cpp
@@ -1,7 +1,7 @@
 // Sort two times.
 // First sort small to big
 // Second sort by abs(i-m), and arr[i] > arr[j]
-// O(NlogN)
+// O(NlogN) + O(NlogN)
 
 class Solution {
 public:
@@ -17,4 +17,25 @@ public:
 };
 
 // Can be Optimal.
-// Second sort can use Two Pointer
+// Second sort can use Two Pointer, Very Fast
+// O(NlogN)
+class Solution {
+public:
+    vector<int> getStrongest(vector<int>& arr, int k) {
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        int m = arr[(n-1)/2];
+        vector<int> res;
+        int i = 0, j = arr.size()-1;
+        while(res.size() < k){
+            if(abs(arr[i] - m) > abs(arr[j] - m))
+                res.push_back(arr[i++]);
+            else
+                res.push_back(arr[j--]);
+        }
+        return res;
+    }
+};
+
+// use quick select algorithm get mid, and maintain queue.
+// O(nlogk)
