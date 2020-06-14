92521e235f150dd989c5bb638ed7dc193cacfc85
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-06-14 14:46:38 +0800

    O(N), Monotone Stack

--- Array/1475. Final Prices With a Special Discount in a Shop.cpp
+++ Array/1475. Final Prices With a Special Discount in a Shop.cpp
@@ -16,4 +16,19 @@ public:
     }
 };
 
-// O(N). Stack
+// O(N). Monotone Stack
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        int n = prices.size();
+        stack<int> st;
+        for(int i = 0; i < n; ++i){
+            while(!st.empty() && prices[st.top()] >= prices[i]){
+                prices[st.top()] -= prices[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        return prices;
+    }
+};
