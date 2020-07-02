f045aaed28d87c4f9cfc05c44607fbaebddbc935
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-07-02 15:17:40 +0800

    BFS.

---
+++ BFS/107. Binary Tree Level Order Traversal II.cpp
@@ -0,0 +1,35 @@
+// Just A Classic BFS. O(N)
+// check the empty case.
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        if(!root) return {};
+        vector<vector<int>> res;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            vector<int> level;
+            for(int s = q.size()-1; s >= 0; --s){
+                auto cur = q.front(); q.pop();
+                level.push_back(cur->val);
+                if(cur->left) q.push(cur->left);
+                if(cur->right) q.push(cur->right);
+            }
+            res.push_back(level);
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
