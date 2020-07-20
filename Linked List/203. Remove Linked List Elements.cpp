7ba3df330ea863f4818c797d20756962e392b772
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-07-20 15:48:03 +0800

    Just Remove

---
+++ Linked List/203. Remove Linked List Elements.cpp
@@ -0,0 +1,28 @@
+// Not Optimal, But simple. memory leak maybe. no delete memory
+
+/*
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        while(head && head->val == val) head = head->next;
+        ListNode* tmp = head;
+        while(tmp){
+            ListNode* nxt = tmp->next;
+            while(nxt && nxt->val == val){
+                nxt = nxt->next;
+            }
+            tmp->next = nxt;
+            tmp = tmp->next;
+        }
+        return head;
+    }
+};
