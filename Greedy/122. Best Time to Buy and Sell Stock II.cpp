0ff31d715f77a9e758511ebede88f4ff9a57d960
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-07-29 17:46:14 +0800

    Greedy Peek Valley or Dp

--- Greedy/122. Best Time to Buy and Sell Stock II.cpp
+++ Greedy/122. Best Time to Buy and Sell Stock II.cpp
@@ -1,4 +1,4 @@
-//Can sell then sell. Get price.
+// Autually is because the big diff always not > small diff
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
@@ -9,5 +9,21 @@ public:
             }
         }
         return cash;
+    }
+};
+
+// O(N) dp, same as cooldown.
+// always get maximum. like fib.
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int b0 = -prices[0], b1 = b0;
+        int s0 = 0, s1 = 0;
+        for(int i = 1; i < prices.size(); ++i){
+            b0 = max(b1, s1 - prices[i]);
+            s0 = max(s1, b1 + prices[i]);
+            b1 = b0, s1 = s0;
+        }
+        return s0;
     }
 };
