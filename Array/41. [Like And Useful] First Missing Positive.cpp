ef8a91a92c8fe2048237356c94da2867a9711878
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-09-30 19:11:56 +0800

    Tricky, Fun

---
+++ Array/41. [Like And Useful] First Missing Positive.cpp
@@ -0,0 +1,18 @@
+// Fun, Tricky
+// So jump until the position all correct.
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i < n; ++i){
+            while(nums[i] > 0 && nums[i] <= n && nums[nums[i]-1] != nums[i]) 
+                swap(nums[i], nums[nums[i]-1]);
+        }
+        
+        for(int i = 0; i < n; ++i){
+            if(nums[i] != i+1) return i+1;
+        }
+        
+        return n+1;
+    }
+};
