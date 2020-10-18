f091b01ba2303301cd5c1562bc2b36db09232508
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-10-18 14:35:23 +0800

    O(Nsqrt(N)) or O(N*hn). like prime test.

--- Union Find/1627. Graph Connectivity With Threshold.cpp
+++ Union Find/1627. Graph Connectivity With Threshold.cpp
@@ -32,3 +32,34 @@ public:
         return res;
     }
 };
+
+
+// Some optimal.
+// just like prime test. each i go to 2*i test.
+class Solution {
+public:
+    int p[10005];
+    int find(int x){
+        return p[x] < 0 ? x : p[x] = find(p[x]);
+    }
+    void uni(int a, int b){
+        a = find(a), b = find(b);
+        if(a == b) return;
+        p[b] = a;
+    }
+    vector<bool> areConnected(int n, int threshold, vector<vector<int>>& queries) {
+        memset(p, -1, sizeof p);
+        for(int i = 1; i <= n; ++i){
+            for(int j = 2*i; j <= n; j += i){
+                if(i > threshold) uni(j, i);
+            }
+        }
+        vector<bool> res;
+        for(auto& q: queries){
+            int a = find(q[0]), b = find(q[1]);
+            res.push_back(a == b);
+        }
+        
+        return res;
+    }
+};
