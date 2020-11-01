0a779b6f0f399ca77513ab4b80e3886a68a250a3
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-11-01 10:22:27 +0800

    Enum O(N^3)

--- String/1638. Count Substrings That Differ by One Character.cpp
+++ String/1638. Count Substrings That Differ by One Character.cpp
@@ -1,5 +1,5 @@
 // Just Brute Force.
-// O(N^3).
+// O(N^4).
 
 class Solution {
 public:
@@ -22,3 +22,21 @@ public:
     }
 };
 
+// O(N^3). Enum all position of s and t. and just compare
+class Solution {
+public:
+    int countSubstrings(string s, string t) {
+        int res = 0, n = s.size();
+        for(int i = 0; i < s.size(); ++i){
+            for(int j = 0; j < t.size(); ++j){
+                int diff = 0;
+                for(int pos = 0; i+pos < s.size() && j+pos < t.size(); ++pos){
+                    if (s[i + pos] != t[j + pos] && ++diff > 1)
+                        break;
+                    res += diff;
+                }
+            }
+        }
+        return res;
+    }
+};
