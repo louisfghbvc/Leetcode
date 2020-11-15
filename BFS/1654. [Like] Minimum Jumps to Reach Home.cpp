b7b595991f216ac09573219675498938d8cb685a
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-11-15 14:05:47 +0800

    bfs. tricky.

--- BFS/1654. [Like] Minimum Jumps to Reach Home.cpp
+++ BFS/1654. [Like] Minimum Jumps to Reach Home.cpp
@@ -31,3 +31,30 @@ public:
 
 // Standard bfs. visited need 2D, make sure no ignore case.
 // O(N).
+
+class Solution {
+public:
+    unordered_set<int> gg;
+    int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
+        bool vis[40005][2] = {};
+        gg = unordered_set<int>(forbidden.begin(), forbidden.end());
+        int res = -1;
+        
+        queue<pair<int, bool>> q;
+        q.push({0, 0});
+        int ans = 0;
+        while(q.size()){
+            for(int s = q.size()-1; s >= 0; s--){
+                auto [u, gob] = q.front(); q.pop();
+                if(u > 20000) continue;
+                if(vis[u][gob]) continue;
+                vis[u][gob] = 1;
+                if(u == x) return ans;
+                if(!gg.count(u+a)) q.push({u+a, 0});
+                if(u - b > 0 && !gob && !gg.count(u-b)) q.push({u-b, 1});
+            }
+            ans++;
+        }
+        return -1;
+    }
+};
