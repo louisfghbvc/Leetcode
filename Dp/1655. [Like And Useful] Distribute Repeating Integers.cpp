f5b6c9ac9297385e3f4a57283bcb74707e983d50
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-11-17 10:26:22 +0800

    BitMask Dp & backtracking. O(N3^M). Tricky and Hard.
    Hard. but can just backtracking. N = 50. M = 10.

--- Dp/1655. [Like And Useful] Distribute Repeating Integers.cpp
+++ Dp/1655. [Like And Useful] Distribute Repeating Integers.cpp
@@ -70,3 +70,87 @@ public:
         return dfs(0, 0);
     }
 };
+
+// Bottom - up clear and maybe easy to understand.
+// O(n3^m). dp can choose i or not choose i.
+// n = arr.size, m = quantity.size()
+// base is dp[n][(1<<m)-1] = 1
+// so dp[i][j] = dp[i+1][j]. if not choose i. j just from next mask.
+// and choose i need to consider all (Not mask) of submask.
+// means j = 1 can not use. use 0. think j = 10101. so submask is from 01010.
+// if ok[i][submask]. means submask is small than arr[i] don't consider i+1 sum. just consider current sum.
+// so can choose. dp[i][j] = dp[i+1][j|submask].
+
+class Solution {
+public:
+    vector<int> arr;
+    bool dp[55][1<<10];
+    bool ok[55][1<<10];
+    int n, m;
+    
+    bool canDistribute(vector<int>& nums, vector<int>& quantity) {
+        memset(dp, 0, sizeof dp);
+        memset(ok, 0, sizeof ok);
+        unordered_map<int, int> mp;
+        for(auto &x: nums) mp[x]++;
+        for(auto &[a, b]: mp) arr.push_back(b);
+
+        n = arr.size();
+        m = quantity.size();
+        dp[n][(1<<m)-1] = 1;
+        
+        for(int i = n-1; i >= 0; --i){
+            for(int j = 0; j < 1<<m; ++j){
+                int sum = 0;
+                for(int k = 0; k < m; ++k){
+                    if((1<<k)&j) sum += quantity[k];
+                }
+                if(sum <= arr[i]) ok[i][j] = true;
+            }
+            for(unsigned int j = 0; j < 1<<m; ++j){
+                dp[i][j] = dp[i+1][j];
+                unsigned int mask = (~j) & ((1<<m)-1);
+                for(int submask = mask; submask; submask = (submask-1)&mask){
+                    if(ok[i][submask]) dp[i][j] |= dp[i+1][j|submask];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+};
+
+// Bottom up O(nm3^m). slow. different from above is when generate submask
+// loop m.
+class Solution {
+public:
+    vector<int> arr;
+    bool dp[55][1<<10];
+    int n, m;
+    
+    bool canDistribute(vector<int>& nums, vector<int>& quantity) {
+        memset(dp, 0, sizeof dp);;
+        unordered_map<int, int> mp;
+        for(auto &x: nums) mp[x]++;
+        for(auto &[a, b]: mp) arr.push_back(b);
+
+        n = arr.size();
+        m = quantity.size();
+        dp[n][(1<<m)-1] = 1;
+        
+        for(int i = n-1; i >= 0; --i){
+            for(unsigned int j = 0; j < 1<<m; ++j){
+                dp[i][j] = dp[i+1][j];
+                unsigned int mask = (~j) & ((1<<m)-1);
+                for(int submask = mask; submask; submask = (submask-1)&mask){
+                    int sum = 0;
+                    for(int k = 0; k < m; ++k){
+                        if((1<<k)&submask) sum += quantity[k];
+                    }
+                    if(sum <= arr[i]) dp[i][j] |= dp[i+1][j|submask];
+                }
+            }
+        }
+        
+        return dp[0][0];
+    }
+};
