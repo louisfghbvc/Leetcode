b0244bcad7e6d73e6baa7c9fab529f4e00e67274
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-01-01 09:59:58 +0800

    Stack O(N).

--- Stack/84. [Like And Useful] Largest Rectangle in Histogram.cpp
+++ Stack/84. [Like And Useful] Largest Rectangle in Histogram.cpp
@@ -19,3 +19,43 @@ public:
 };
 
 // Also can use segment tree.
+
+
+
+
+// Solution3 use left and right array like kmp. just jump and jump from left and right.
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        if(heights.empty()) return 0;
+        int n = heights.size();
+        vector<int> left(n);
+        vector<int> right(n);
+        
+        left[0] = -1;
+        right[n-1] = n;
+        
+        for(int i = 1; i < n; ++i){
+            int p = i-1;
+            while(p >= 0 && heights[p] >= heights[i]){
+                p = left[p];
+            }
+            left[i] = p;
+        }
+        
+        for(int i = n-2; i >= 0; --i){
+            int p = i+1;
+            while(p < n && heights[p] >= heights[i]){
+                p = right[p];
+            }
+            right[i] = p;
+        }
+        
+        int res = 0;
+        for(int i = 0; i < n; ++i){
+            res = max(res, heights[i] * (right[i] - left[i] - 1));
+        }
+        
+        return res;
+    }
+};
