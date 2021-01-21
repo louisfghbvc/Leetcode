150cc9d80330d144f378e7329fa0e4cc654c6540
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-01-21 15:13:00 +0800

    O(N^2). O(N) Manacher.

--- Dp/5-longest-palindromic-substring.cpp
+++ Dp/5-longest-palindromic-substring.cpp
@@ -32,3 +32,43 @@ public:
         return s.substr(l, r - l + 1);
     }
 };
+
+// O(N). Manacher's Algorithm. Tricky.
+// just concern i or i + dp[mir] is in R.
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string t = "$#";
+        for(char c: s){
+            t += c;
+            t += "#";
+        }
+        t += '^';
+        int n = t.size();
+        
+        vector<int> dp(n);
+        int C = 0, R = 0;
+        int mx = 0, mxi = 0;
+        
+        for(int i = 1; i+1 < n; ++i){
+            int mir = 2*C-i;
+            if(i < R && mir >= 1 && (i + dp[mir]) < R){
+                dp[i] = dp[mir];
+                continue;
+            }
+            while(t[i - 1 - dp[i]] == t[i + 1 + dp[i]]) dp[i]++;
+            
+            if(i + dp[i] > R){
+                R = i+dp[i];
+                C = i;
+            }
+            
+            if(dp[i] > mx){
+                mx = dp[i];
+                mxi = i;
+            }
+        }
+        
+        return s.substr((mxi - mx)/2, mx);
+    }
+};
