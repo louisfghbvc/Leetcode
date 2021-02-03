195ff1e31c1675a6db0dced9f3cb0864824238c3
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-02-03 20:47:50 +0800

    O(N). Floyd cycle.

---
+++ Two Pointers/141. Linked List Cycle.cpp
@@ -0,0 +1,16 @@
+// O(N). use deep equal. cuz value may be duplicate.
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head || !head->next) return false;
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) return true;
+        }
+        return false;
+    }
+};
