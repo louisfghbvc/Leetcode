5342703a8b1b87b356c01b7d138d10be7beaec4c
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-02-07 16:16:32 +0800

    Meet in the middle.

--- Divide And Conquer/1755. [Like And Useful] Closest Subsequence Sum.cpp
+++ Divide And Conquer/1755. [Like And Useful] Closest Subsequence Sum.cpp
@@ -39,3 +39,40 @@ public:
         return mn;
     }
 };
+
+
+// O(N2^N). use vector more fast.
+class Solution {
+public:
+    int minAbsDifference(vector<int>& nums, int goal) {
+        
+        int n = nums.size();
+        vector<int> st;
+        int b = n/2;
+        int r = n - b;
+        
+        int mn = INT_MAX;
+        for(int S = 0; S < (1<<b); ++S){
+            int tmp = 0;
+            for(int j = 0; j < b; ++j) if(S&(1<<j)) tmp += nums[j];
+            st.push_back(tmp);
+            mn = min(mn, abs(tmp-goal));
+        }
+        sort(st.begin(), st.end());
+        
+        for(int S = 0; S < 1<<r; ++S){
+            int tmp = 0;
+            for(int j = 0; j < r; ++j) if(S&(1<<j)) tmp += nums[j+b];
+            
+            mn = min(mn, abs(tmp-goal));
+            int pos = lower_bound(st.begin(), st.end(), goal-tmp) - st.begin();
+            for(int j = pos-1; j <= pos; ++j) {
+                if(j >= 0 && j < st.size()) {
+                    mn = min(mn, abs(st[j]+tmp - goal));
+                }
+            }
+        }
+        
+        return mn;
+    }
+};
