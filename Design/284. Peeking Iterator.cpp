2b8205830d1167fe52a2624fd8a40490b6f0db05
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-02-08 17:28:00 +0800

    Boring design. oop

---
+++ Design/284. Peeking Iterator.cpp
@@ -0,0 +1,83 @@
+// OK.. wtf..
+/*
+ * Below is the interface for Iterator, which is already defined for you.
+ * **DO NOT** modify the interface for Iterator.
+ *
+ *  class Iterator {
+ *		struct Data;
+ * 		Data* data;
+ *		Iterator(const vector<int>& nums);
+ * 		Iterator(const Iterator& iter);
+ *
+ * 		// Returns the next element in the iteration.
+ *		int next();
+ *
+ *		// Returns true if the iteration has more elements.
+ *		bool hasNext() const;
+ *	};
+ */
+
+class PeekingIterator : public Iterator {
+public:
+    int val;
+    bool hasnext;
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+        hasnext = Iterator::hasNext();
+        if(hasnext) val  = Iterator::next();
+	}
+	
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+        return val; 
+	}
+	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+        int tmp = val;
+	    hasnext = Iterator::hasNext();
+        if(hasnext) val = Iterator::next();
+        return tmp;
+	}
+	
+	bool hasNext() const {
+	    return hasnext;
+	}
+};
+
+// one line...
+/*
+ * Below is the interface for Iterator, which is already defined for you.
+ * **DO NOT** modify the interface for Iterator.
+ *
+ *  class Iterator {
+ *		struct Data;
+ * 		Data* data;
+ *		Iterator(const vector<int>& nums);
+ * 		Iterator(const Iterator& iter);
+ *
+ * 		// Returns the next element in the iteration.
+ *		int next();
+ *
+ *		// Returns true if the iteration has more elements.
+ *		bool hasNext() const;
+ *	};
+ */
+
+class PeekingIterator : public Iterator {
+public:
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+	}
+	
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+        return Iterator(*this).next(); 
+	}
+	
+};
