16eefac748d8231d02b1a82f7efbda1d5310bfce
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-02-12 16:07:28 +0800

    Bit tricky.

---
+++ Bit Manipulation/1342. Number of Steps to Reduce a Number to Zero.cpp
@@ -0,0 +1,15 @@
+// remember to substract 1.
+// O(logN).
+
+class Solution {
+public:
+    int numberOfSteps (int num) {
+        if(!num) return 0;
+        int res = 0;
+        while(num){
+            res += (num & 1) ? 2 : 1;
+            num >>= 1;
+        }
+        return res-1;
+    }
+};
