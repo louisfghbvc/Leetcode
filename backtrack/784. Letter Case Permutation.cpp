a28b3c088435cd41b16b7a6d3a8e4a5c0691fdd4
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-02-17 09:51:03 +0800

    Backtracking, Bit manipulation

--- backtrack/784. Letter Case Permutation.cpp
+++ backtrack/784. Letter Case Permutation.cpp
@@ -1,28 +1,22 @@
+// Bit magic. small letter want to -32. big want to +32.
+// so use XOR.
 class Solution {
-    bool isUpper(char c){
-        if(c >= 'A' && c <= 'Z')
-            return true;
-        return false;
-    }
-    void dfs(vector<string> &res, string t, string &s, int i, int &len){
-        if(i == len){
-            res.push_back(t);
-            return;
-        }
-        if(s[i] >= '0' && s[i] <= '9'){
-            dfs(res, t+s[i], s, i+1, len);
-            return;
-        }
-        if(isUpper(s[i])) 
-            s[i] = s[i] - 'A' + 'a';
-        dfs(res, t + (char)(s[i]-'a' + 'A'), s, i+1, len);
-        dfs(res, t + s[i], s, i+1, len);
-    }
 public:
+    vector<string> res;
+    void dfs(string &s, int i, string cur){
+        if(i == s.size()){
+            res.push_back(cur);
+            return;
+        }
+        if(isdigit(s[i]))
+           dfs(s, i+1, cur + s[i]);
+        else{
+            dfs(s, i+1, cur + s[i]);
+            dfs(s, i+1, cur + char(s[i] ^ (1<<5)));
+        }
+    }
     vector<string> letterCasePermutation(string S) {
-        vector<string> ans;
-        int n = S.size();
-        dfs(ans, "", S, 0, n);
-        return ans;
+        dfs(S, 0, "");
+        return res;
     }
 };
