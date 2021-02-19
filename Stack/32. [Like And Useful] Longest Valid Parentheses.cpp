e9fbeda5c0a29c971e531f97eae1fa88531da549
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-02-19 16:41:16 +0800

    Index Stack, Trick

--- Stack/32. [Like And Useful] Longest Valid Parentheses.cpp
+++ Stack/32. [Like And Useful] Longest Valid Parentheses.cpp
@@ -26,3 +26,41 @@ public:
         return res;
     }
 };
+
+// O(N) index stack, more intuitive.
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n = s.size();
+        int res = 0;
+        
+        stack<int> st;
+        
+        for(int i = 0; i < n; ++i){
+            if(s[i] == '('){
+                st.push(i);
+            }    
+            else{
+                if(st.empty() || s[st.top()] != '('){
+                    st.push(i);
+                }    
+                else{
+                    st.pop();
+                }
+            }
+        }
+        
+        if(st.empty()) res = n;
+        else{
+            int r = n;
+            while(st.size()){
+                res = max(res, r - st.top() - 1);
+                r = st.top();
+                st.pop();
+            }
+            // last to begin.
+            res = max(res, r);
+        }
+        return res;
+    }
+};
