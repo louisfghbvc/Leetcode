a8323b8816d2b3e9ed7de1977e47d3c943787897
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-02-21 12:30:05 +0800

    Classic dp.

--- Dp/516. [Like]Longest Palindromic Subsequence.cpp
+++ Dp/516. [Like]Longest Palindromic Subsequence.cpp
@@ -18,3 +18,29 @@ public:
         return dp[n][n];
     }
 };
+
+
+// O(N^2). another dp. more intuitive.
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n));
+        
+        for(int i = 0; i < n; ++i) dp[i][i] = 1;
+        for(int l = 2; l <= n; ++l){
+            for(int i = 0; i+l-1 < n; ++i){
+                int j = i+l-1;
+                if(s[i] == s[j] && l == 2) dp[i][j] = 2;
+                else if(s[i] == s[j]){
+                    dp[i][j] = dp[i+1][j-1]+2;
+                }
+                else{
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        return dp[0][n-1];
+    }
+};
