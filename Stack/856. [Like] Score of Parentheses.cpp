d028a0e1113851e948e7c75d2a7c1b3fe6df5b7d
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-02-25 01:00:43 +0800

    Stack trick

--- Stack/856. [Like] Score of Parentheses.cpp
+++ Stack/856. [Like] Score of Parentheses.cpp
@@ -1,20 +1,19 @@
+// O(N).
 class Solution {
 public:
     int scoreOfParentheses(string S) {
         stack<int> st;
-        st.push(0);
+        int cur = 0;
         for(char c: S){
             if(c == '('){
-                st.push(0);
+                st.push(cur);
+                cur = 0;
             }
             else{
-                int tmp = st.top(); st.pop();
-                int val = 0;
-                if(tmp>0) val = tmp*2;
-                else val = 1;
-                st.top() += val;
+                cur = st.top() + max(2*cur, 1);
+                st.pop();
             }
         }
-        return st.top();
+        return cur;
     }
 };
