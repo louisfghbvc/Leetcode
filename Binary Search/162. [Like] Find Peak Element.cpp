e98f9331209a33e7b66d9772f4c069bed79e245a
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-03-01 23:50:43 +0800

    BS.

---
+++ Binary Search/162. [Like] Find Peak Element.cpp
@@ -0,0 +1,15 @@
+// Tricky.
+// 3 case. find the pattern
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int l = 0, r = nums.size()-1;
+        while(l < r){
+            int mid = l + (r-l) / 2;
+            if(nums[mid] > nums[mid+1]) r = mid;
+            else l = mid+1;
+        }
+        return l;
+    }
+};
