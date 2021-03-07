4697ad9644efd02ca976ea1c7bfe4814db9bf7f2
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-03-07 22:39:16 +0800

    Line, De morgan

--- Math/836. [Like] Rectangle Overlap.cpp
+++ Math/836. [Like] Rectangle Overlap.cpp
@@ -1,9 +1,14 @@
-// Math. if not overlap must be left or right or top or bottom side
+// Math. if not overlap must be left or right or top or bottom side. update.
 // De morgan
 // O(1)
 class Solution {
 public:
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+        // line must false
+        if(rec1[0] == rec1[2] || rec1[1] == rec1[3] ||
+           rec2[0] == rec2[2] || rec2[1] == rec2[3]
+          ) return false;
+        
         return !(rec1[2] <= rec2[0] || rec1[0] >= rec2[2]
                 || rec1[3] <= rec2[1] || rec1[1] >= rec2[3]
                );
