5eaaf938c0a357b2a0b3088473a674fc3260f58b
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-03-18 20:02:42 +0800

    DP, LIS tweak. trick

--- Dp/376. [Like And Useful] Wiggle Subsequence.cpp
+++ Dp/376. [Like And Useful] Wiggle Subsequence.cpp
@@ -13,3 +13,27 @@ public:
         return max(up, down);
     }
 };
+
+
+// LIS tweak. but only consider two case, neg and positive.
+// O(N)
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dif(n-1);
+        for(int i = 1; i < n; ++i) dif[i-1] = nums[i] - nums[i-1];
+        
+        int pos = 0, neg = 0;
+        for(int i = 0; i < n-1; ++i){
+            if(dif[i] > 0){
+                pos = max(pos, neg+1);
+            }
+            if(dif[i] < 0){
+                neg = max(neg, pos+1);
+            }
+        }
+        
+        return max(pos, neg) + 1;
+    }
+};
