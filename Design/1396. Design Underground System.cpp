06bf96b4eed7fc63e1bb8f2cd6c26939ba0b7db6
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-03-20 18:40:50 +0800

    Two map, Nested map

--- Design/1396. Design Underground System.cpp
+++ Design/1396. Design Underground System.cpp
@@ -27,10 +27,33 @@ public:
     }
 };
 
-/**
- * Your UndergroundSystem object will be instantiated and called as such:
- * UndergroundSystem* obj = new UndergroundSystem();
- * obj->checkIn(id,stationName,t);
- * obj->checkOut(id,stationName,t);
- * double param_3 = obj->getAverageTime(startStation,endStation);
- */
+// Concate string.
+class UndergroundSystem {
+public:
+    
+    unordered_map<string, pair<int, int>> mp;
+    unordered_map<int, pair<string, int>> user;
+    
+    UndergroundSystem() {
+        
+    }
+    
+    void checkIn(int id, string stationName, int t) {
+        user[id] = {stationName, t};
+    }
+    
+    void checkOut(int id, string stationName, int t) {
+        auto &pre = user[id];
+        string route = pre.first + ">" + stationName;
+        auto &cur = mp[route];
+        cur.first += t - pre.second;
+        cur.second++;
+    }
+    
+    double getAverageTime(string startStation, string endStation) {
+        string route = startStation + ">" + endStation;
+        auto &cur = mp[route];
+        return (double)cur.first / cur.second;
+    }
+};
+
