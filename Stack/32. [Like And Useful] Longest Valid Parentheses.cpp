beafa9bcf72283629bdd803e9e248bda3a371774
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-04-03 19:28:23 +0800

    index stack / tricky dp.

--- Stack/32. [Like And Useful] Longest Valid Parentheses.cpp
+++ Stack/32. [Like And Useful] Longest Valid Parentheses.cpp
@@ -64,3 +64,30 @@ public:
         return res;
     }
 };
+
+
+// Dp. check ')'. two case.
+
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n = s.size();
+        vector<int> dp(n);
+        
+        int mx = 0;
+        for(int i = 1; i < n; ++i){
+            if(s[i] == ')'){
+                if(s[i-1] == '(') dp[i] = (i-2 < 0 ? 0 : dp[i-2]) + 2;
+                else{ // i-1 = ')'
+                    if(i - dp[i-1] > 0 && s[i - dp[i-1] - 1] == '('){ 
+                        int prev = i-dp[i-1]-2 >= 0 ? dp[i-dp[i-1]-2] : 0;
+                        dp[i] = dp[i-1] + prev + 2;
+                    } 
+                }
+            }
+            mx = max(mx, dp[i]);
+        }
+        
+        return mx;
+    }
+};
