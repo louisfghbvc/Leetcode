464a2a30129d6468f34f495848c24228c60acfc8
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-04-05 00:43:06 +0800

    Simple.

---
+++ String/1816. Truncate Sentence.cpp
@@ -0,0 +1,13 @@
+// So clean, no need to split and count.
+// O(N).
+
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        for(int i = 0; i < s.size(); ++i){
+            if(s[i] == ' ' && --k == 0)
+                return s.substr(0, i);
+        }
+        return s;
+    }
+};
