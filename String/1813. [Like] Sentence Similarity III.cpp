bfcb5a102724f01bc552ce9110767b5c365d3d28
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-04-05 17:35:55 +0800

    Compare.

---
+++ String/1813. [Like] Sentence Similarity III.cpp
@@ -0,0 +1,40 @@
+// O(N?). Compare and calculate distance count > 0
+
+class Solution {
+public:
+    vector<string> ToA(string &s){
+        s += ' ';
+        vector<string> res;
+        string t = "";
+        for(char c: s){
+            if(c == ' '){
+                res.push_back(t);
+                t = "";
+            }  
+            else t += c;
+        }
+        return res;
+    }
+    bool areSentencesSimilar(string sentence1, string sentence2) {
+        vector<string> A = ToA(sentence1);
+        vector<string> B = ToA(sentence2);
+        if(A.size() < B.size()) swap(A, B);
+        A.push_back(" ");
+        B.push_back(" ");
+        vector<int> same;
+        same.push_back(-1);
+        int j = 0;
+        for(int i = 0; i < A.size() && j < B.size(); ++i){
+            if(A[i] == B[j]){
+                j++;
+                same.push_back(i);
+            }
+        }
+        if(j != B.size()) return false;
+        int cnt = 0;
+        for(int i = 1; i < same.size(); ++i){
+            if(same[i] - same[i-1] - 1 > 0) cnt++;
+        }
+        return cnt <= 1;
+    }
+};
