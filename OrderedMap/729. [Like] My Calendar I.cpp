504dddf08ffb4780c90321da337d0a85608e2a4d
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-04-06 23:21:32 +0800

    BS.

---
+++ OrderedMap/729. [Like] My Calendar I.cpp
@@ -0,0 +1,27 @@
+// O(NlogN). always check closest possible enough.
+
+class MyCalendar {
+public:
+    map<int, int> mp;
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end) {
+        auto p = mp.lower_bound(start);
+        if(p != mp.end()){
+            if(end > p->first) return false;
+        }
+        if(p != mp.begin()){
+            if(start < prev(p)->second) return false;
+        }
+        mp[start] = end;
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
