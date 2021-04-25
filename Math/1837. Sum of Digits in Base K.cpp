8fa87786aa9aef6f369f586989d572959cdfe1fa
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-04-25 18:12:15 +0800

    Simple base.

---
+++ Math/1837. Sum of Digits in Base K.cpp
@@ -0,0 +1,13 @@
+// Simple. just math
+
+class Solution {
+public:
+    int sumBase(int n, int k) {
+        int res = 0;
+        while(n){
+            res += n % k;
+            n /= k;
+        }
+        return res;
+    }
+};
