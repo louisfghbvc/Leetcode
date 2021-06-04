71e053474c6a0e6c28fc1158e13120cea323a3c1
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-06-04 18:36:04 +0800

    BFS / Bi - BFS.

--- BFS/752. [Like] Open the Lock.cpp
+++ BFS/752. [Like] Open the Lock.cpp
@@ -35,4 +35,54 @@ public:
     }
 };
 
-// Bidirectional BFS. more fast.
+// Bidirectional BFS. more fast. 8ms
+
+class Solution {
+public:
+    int openLock(vector<string>& deadends, string target) {
+        vector<int> v1(10000);
+        vector<int> v2(10000);
+        
+        for(auto &s: deadends){
+            v1[stoi(s)] = v2[stoi(s)] = -1;
+        }
+        
+        int start = 0, end = stoi(target);
+        if(v1[start] == -1) return -1;
+        if(start == end) return 0;
+        
+        queue<int> q1, q2;
+        q1.push(start);
+        q2.push(end);
+        v1[start] = v2[end] = 1;
+        
+        vector<int> base = {1, 10, 100, 1000};
+        
+        int s1 = 0, s2 = 0;
+        while(q1.size() && q2.size()){
+            s1++;
+            
+            int size = q1.size();
+            for(int S = 0; S < size; ++S){
+                int u = q1.front(); q1.pop();
+                for(int i = 0; i < 4; ++i){
+                    int d = (u / base[i]) % 10;
+                    for(int nd: {(d + 1) % 10, (d + 9) % 10}){
+                        int nxt = u - d * base[i] + nd * base[i];
+                        if(v2[nxt] == 1) return s1 + s2;
+                        if(v1[nxt]) continue;
+                        q1.push(nxt);
+                        v1[nxt] = 1;
+                    }
+                }
+                
+            }
+            
+            swap(q1, q2);
+            swap(v1, v2);
+            swap(s1, s2);
+        }
+        
+        return -1;
+    }
+};
