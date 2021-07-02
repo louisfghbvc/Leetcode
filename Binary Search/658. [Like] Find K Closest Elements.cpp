0aeb8a5776a0bcdbdb69033304a6ec3763727aa6
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-07-02 20:49:38 +0800

    BS / Two Pointer

---
+++ Binary Search/658. [Like] Find K Closest Elements.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int n = arr.size();
+        int l = 0, r = n-k;
+        
+        while(l < r){
+            int mid = (l+r)/2;
+            if(x - arr[mid] > arr[mid + k] - x) l = mid + 1;
+            else r = mid;
+        }
+    
+        return vector<int>(arr.begin()+l, arr.begin()+l+k);
+    }
+};
+
+
+// O(N). Two Pointers.
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int n = arr.size();
+        int l = 0, r = n-1;
+        
+        while(r - l + 1 > k){
+            if(abs(arr[l] - x) > abs(arr[r] - x)) l++;
+            else r--;
+        }
+    
+        return vector<int>(arr.begin()+l, arr.begin()+r+1);
+    }
+};
