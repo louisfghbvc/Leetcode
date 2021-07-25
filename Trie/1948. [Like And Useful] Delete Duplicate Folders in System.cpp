b4b21f1690f46e1b758029bc156ea42c6cce6c04
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-07-25 22:12:09 +0800

    String Trie + DFS + Hashing. Tricky.

--- Trie/1948. [Like And Useful] Delete Duplicate Folders in System.cpp
+++ Trie/1948. [Like And Useful] Delete Duplicate Folders in System.cpp
@@ -61,3 +61,70 @@ public:
         return res;
     }
 };
+
+
+// use hashing int. same runtime. but space is good.
+class Solution {
+    vector<vector<string>> res;
+    typedef unsigned long long ull;
+    const int P = 129; // since ascii is 128
+public:
+    struct trie_node{
+        string name;
+        map<string, trie_node*> next;
+        bool isDel = false;
+        ull hash = 0;
+        trie_node(string n = "") : name(n) {};
+    };
+    
+    void add(trie_node* root, vector<string> &path){
+        trie_node* tmp = root;
+        for(auto &s: path){
+            if(!tmp->next[s]) tmp->next[s] = new trie_node(s);
+            tmp = tmp->next[s];
+        }
+    }
+    
+    unordered_map<ull, trie_node*> seen;
+    void dedupe(trie_node* root){ // post-order, similar to LC 652
+        string subFolder = "";
+        for(auto &[name, next]: root->next){
+            dedupe(next);
+            subFolder += name + "," + to_string(next->hash);
+        }
+        for(char c: subFolder)
+            root->hash = root->hash * P + c;
+        if(root->next.size()){ // not leaf
+            if(seen.count(root->hash))
+                seen[root->hash]->isDel = root->isDel = true;
+            else
+                seen[root->hash] = root;
+        }
+    }
+    
+    vector<string> path;
+    void gen_path(trie_node* root){
+        if(root->isDel) return;
+        path.push_back(root->name);
+        res.push_back(path);
+        for(auto &[_, next]: root->next){
+            gen_path(next);
+        }
+        path.pop_back();
+    }
+    
+    vector<vector<string>> deleteDuplicateFolder(vector<vector<string>>& paths) {
+        // 1. build tree
+        // 2. dedupe
+        // 3. parse ans
+        
+        trie_node* root = new trie_node();
+        for(auto &p: paths)
+            add(root, p);
+        dedupe(root);
+        for(auto &[_, next]: root->next)
+            gen_path(next);
+        
+        return res;
+    }
+};
