b8af6310240b0bfbfa5262543a1df25e75150b9f
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-08-03 19:22:31 +0800

    Backtracking

--- backtrack/90. Subsets II.cpp
+++ backtrack/90. Subsets II.cpp
@@ -1,26 +1,23 @@
-//4ms 9.4MB
+// Bakctracking, and compare prev and cur.
+
 class Solution {
 public:
-    void dfs(int st, vector<int>& nums, vector<int> &tmp, vector<vector<int>>&res, vector<bool> &vis){
+    vector<vector<int>> res;
+    vector<int> tmp;
+    
+    void dfs(vector<int> &arr, int i){
         res.push_back(tmp);
-        for(int i = st; i < nums.size(); ++i){
-            if(vis[i]) continue;
-            vis[i] = true;
-            tmp.push_back(nums[i]);
-            dfs(i + 1, nums, tmp, res, vis);
+        for(int j = i; j < arr.size(); ++j){
+            tmp.push_back(arr[j]);
+            dfs(arr, j+1);
             tmp.pop_back();
-            vis[i] = false;
-            while(i < nums.size() - 1 && nums[i] == nums[i + 1]) i++;
+            while(j + 1 < arr.size() && arr[j] == arr[j+1]) j++;
         }
     }
+    
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        
-        vector<vector<int>> res;
-        vector<bool> vis(nums.size(), 0);
-        vector<int> tmp;
-        
-        dfs(0, nums, tmp, res, vis);
+        dfs(nums, 0);
         return res;
     }
 };
