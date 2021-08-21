4389800e71629f900242db1302f0cb4b97505dc3
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-08-21 16:14:17 +0800

    Backtracking

--- backtrack/37. [Like] Sudoku Solver.cpp
+++ backtrack/37. [Like] Sudoku Solver.cpp
@@ -28,3 +28,55 @@ public:
         solveSudoku(board, 0, 0);
     }
 };
+
+
+// use extra space to do O(1) check
+class Solution {
+public:
+    bool row[9][10], col[9][10], sub[9][10];
+    
+    int get(int i, int j){
+        return (i/3) * 3 + j/3; 
+    }
+    
+    bool check(vector<vector<char>>& board, int i, int j){
+        if(row[i][board[i][j] - '0'] ||
+           col[j][board[i][j] - '0'] || 
+           sub[get(i, j)][board[i][j] - '0']) return false;
+        return true;
+    }
+    
+    bool dfs(vector<vector<char>>& board, int i, int j){
+        if(i == 9) return true;
+        if(j == 9) return dfs(board, i+1, 0);
+        if(board[i][j] != '.') return dfs(board, i, j+1);
+            
+        for(int d = 1; d <= 9; ++d){
+            board[i][j] = '0'+d;
+            if(check(board, i, j)){
+                row[i][d] = 1;
+                col[j][d] = 1;
+                sub[get(i, j)][d] = 1;
+                if(dfs(board, i, j+1)) return true;
+                row[i][d] = 0;
+                col[j][d] = 0;
+                sub[get(i, j)][d] = 0;
+            }
+            board[i][j] = '.';
+        }
+        
+        return false;
+    }
+    
+    void solveSudoku(vector<vector<char>>& board) {
+        for(int i = 0; i < 9; ++i){
+            for(int j = 0; j < 9; ++j){
+                if(board[i][j] == '.') continue;
+                row[i][board[i][j] - '0'] = 1;
+                col[j][board[i][j] - '0'] = 1;
+                sub[get(i, j)][board[i][j] - '0'] = 1;
+            }
+        }
+        dfs(board, 0, 0);
+    }
+};
