3e9b9f929a225b46a805738705a20c69704e833e
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-08-22 00:31:31 +0800

    Dp + Dijkstra.

---
+++ Dp/1976. [Like] Number of Ways to Arrive at Destination.cpp
@@ -0,0 +1,43 @@
+// Dp + Dijkstra. Careful.
+
+class Solution {
+public:
+    const long mod = 1e9+7;
+    typedef pair<long, long> ii;
+    int countPaths(int n, vector<vector<int>>& roads) {
+        
+        vector<vector<ii>> g(n);
+        for(auto &r: roads){
+            long u = r[0], v = r[1], w = r[2];
+            g[u].push_back({v, w});
+            g[v].push_back({u, w});
+        }
+        
+        vector<long> dp(n, LLONG_MAX/2); // dis
+        dp[0] = 0;
+        
+        vector<long> dp2(n); // num of way
+        dp2[0] = 1;
+        
+        priority_queue<ii, vector<ii>, greater<ii>> pq; // dist, u
+        pq.push({0, 0});
+        
+        while(pq.size()){
+            auto [cost, u] = pq.top(); pq.pop();
+            if(cost > dp[u]) continue; // careful this, may wrong add dp2
+            
+            for(auto &[v, w]: g[u]){
+                if(dp[v] > dp[u] + w){
+                    dp[v] = dp[u] + w;
+                    dp2[v] = dp2[u];
+                    pq.push({dp[v], v});
+                }
+                else if(dp[v] == dp[u] + w){
+                    dp2[v] = (dp2[v] + dp2[u]) % mod;
+                }
+            }
+        }
+        
+        return dp2[n-1];
+    }
+};
