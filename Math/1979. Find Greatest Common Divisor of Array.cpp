170a1a9f01d380a566141be5f100247ebd92d055
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-08-22 14:33:01 +0800

    Simple

---
+++ Math/1979. Find Greatest Common Divisor of Array.cpp
@@ -0,0 +1,8 @@
+// Simple math. can do O(N).
+class Solution {
+public:
+    int findGCD(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return __gcd(nums[0], nums.back());
+    }
+};
