bad3999380d8a74cdbd1bbd9e1ab4740341bc2e6
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-08-23 15:58:17 +0800

    hashset. dfs

--- Tree/653. Two Sum IV - Input is a BST.cpp
+++ Tree/653. Two Sum IV - Input is a BST.cpp
@@ -21,3 +21,15 @@ public:
         return false;
     }
 };
+
+// use hashset O(N). O(N) clean
+class Solution {
+public:
+    unordered_set<int> st;
+    bool findTarget(TreeNode* root, int k) {
+        if(!root) return false;
+        if(st.count(k - root->val)) return true;
+        st.insert(root->val);
+        return findTarget(root->left, k) || findTarget(root->right, k);
+    }
+};
