a89b04d410f2773d5d3e94d97c1dcfb1f84cb932
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-08-30 20:03:42 +0800

    observe, math

---
+++ Math/598. Range Addition II.py
@@ -0,0 +1,9 @@
+# interesting. O(ops). observe that always min x, y
+class Solution:
+    def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
+        
+        for a, b in ops:
+            m = min(m, a)
+            n = min(n, b)
+        
+        return m*n
