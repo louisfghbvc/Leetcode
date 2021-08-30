d1df7995aa9594e8c39e90c277133b9cc583652f
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-08-30 11:50:43 +0800

    Bitmask, Top down dp, Tricky.

--- Dp/1986. [Like] Minimum Number of Work Sessions to Finish the Tasks.py
+++ Dp/1986. [Like] Minimum Number of Work Sessions to Finish the Tasks.py
@@ -15,4 +15,31 @@ class Solution:
             return res
         return dfs(0, 0)
       
-# O(3^N)
+# O(N2^N) super fast. just use pair to do that (numbers of session, accumulate time).
+# impressive.
+class Solution {
+public:
+    int minSessions(vector<int>& tasks, int sessionTime) {
+        int n = tasks.size();
+        
+        // numbers of session, last time
+        vector<pair<int, int>> dp(1<<n);
+        dp[0] = {1, 0};
+        for(int S = 1; S < 1<<n; ++S){
+            pair<int, int> best = {n+1, 0};
+            for(int i = 0; i < n; ++i){
+                if(S >> i & 1){
+                    auto cur = dp[S ^ (1<<i)];
+                    if(cur.second + tasks[i] <= sessionTime)
+                        cur.second += tasks[i];
+                    else
+                        cur = {cur.first+1, tasks[i]};
+                    best = min(best, cur);
+                }
+            }
+            dp[S] = best;
+        }
+        
+        return dp.back().first;
+    }
+};
