5a930349852c54497dd6172963f80871b3af8319
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-09-01 22:09:24 +0800

    Classic Dp.

--- Dp/256. [Like] Paint House.cpp
+++ Dp/256. [Like] Paint House.cpp
@@ -1,14 +1,14 @@
 // space optimal. T:O(N), S:O(1).
-// do it reverse, and use origin array. awesome
+// use origin array. awesome. just dp.
 class Solution {
 public:
     int minCost(vector<vector<int>>& costs) {
-        for(int i = costs.size()-2; i >= 0; --i){
-            costs[i][0] += min(costs[i+1][1], costs[i+1][2]);
-            costs[i][1] += min(costs[i+1][0], costs[i+1][2]);
-            costs[i][2] += min(costs[i+1][0], costs[i+1][1]);
+        for(int i = 1; i < costs.size(); ++i){
+            costs[i][0] += min(costs[i-1][1], costs[i-1][2]);
+            costs[i][1] += min(costs[i-1][0], costs[i-1][2]);
+            costs[i][2] += min(costs[i-1][0], costs[i-1][1]);
         }
-        return *min_element(costs[0].begin(), costs[0].end());
+        return *min_element(costs.back().begin(), costs.back().end());
     }
 };
 
