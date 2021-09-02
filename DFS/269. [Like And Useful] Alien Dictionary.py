7da8b79e63cd1605a5392ed1bf31a347f8b0d8e5
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-09-02 12:39:28 +0800

    Topological DFS / BFS

--- DFS/269. [Like And Useful] Alien Dictionary.py
+++ DFS/269. [Like And Useful] Alien Dictionary.py
@@ -45,3 +45,46 @@ class Solution:
         # edge case, we must use all char.
         if len(self.res) < len(appear): return ""
         return self.res[::-1]
+
+    
+# BFS
+class Solution:
+
+    def alienOrder(self, words: List[str]) -> str:
+        
+        ind = [0] * 26
+        graph = defaultdict(list)
+        appear = {ord(c) - ord('a') for word in words for c in word}
+        
+        # compare words[i-1], words[i]
+        # build graph
+        for i in range(1, len(words)):
+            j = 0
+            Found = False
+            while j < len(words[i-1]) and j < len(words[i]):
+                # add edge
+                if words[i-1][j] != words[i][j]:
+                    ind[ord(words[i][j]) - ord('a')] += 1
+                    graph[ord(words[i-1][j]) - ord('a')].append(ord(words[i][j]) - ord('a'))
+                    Found = True
+                    break
+                j += 1
+            if not Found and len(words[i-1]) > len(words[i]): return ""
+        
+        q = deque()
+        res = ''
+        for i in appear:
+            if ind[i] == 0:
+                res += chr(i + 97)
+                q.append(i)
+        
+        # topological bfs
+        while q:
+            u = q.popleft()
+            for v in graph[u]:
+                ind[v] -= 1
+                if ind[v] == 0:
+                    res += chr(v + 97)
+                    q.append(v)
+        
+        return res if len(res) == len(appear) else '' 
