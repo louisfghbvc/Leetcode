d7242e45a8513b294b0215edbd18de3cce0203ec
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-09-04 11:51:12 +0800

    Dp, Knapsack tweak.

--- Dp/376. [Like And Useful] Wiggle Subsequence.cpp
+++ Dp/376. [Like And Useful] Wiggle Subsequence.cpp
@@ -1,5 +1,5 @@
 // Dp magic. just compare adjacent.
-// O(N). Space O(1).
+// O(N). Space O(1). From Space O(N) to O(1)
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
@@ -14,26 +14,21 @@ public:
     }
 };
 
-
-// LIS tweak. but only consider two case, neg and positive.
-// O(N)
-class Solution {
-public:
-    int wiggleMaxLength(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dif(n-1);
-        for(int i = 1; i < n; ++i) dif[i-1] = nums[i] - nums[i-1];
+// maintain two dp. up and down.
+// each is choose or not choose. O(N).
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        n = len(nums)
+        up, down = [1] * n, [1] * n
         
-        int pos = 0, neg = 0;
-        for(int i = 0; i < n-1; ++i){
-            if(dif[i] > 0){
-                pos = max(pos, neg+1);
-            }
-            if(dif[i] < 0){
-                neg = max(neg, pos+1);
-            }
-        }
+        for i in range(1, n):
+            # not choose nums[i]
+            up[i], down[i] = up[i-1], down[i-1]
+            
+            # choose nums[i]
+            if nums[i-1] < nums[i]: # up
+                up[i] = max(up[i], down[i-1] + 1)
+            elif nums[i-1] > nums[i]: # down
+                down[i] = max(down[i], up[i-1] + 1)
         
-        return max(pos, neg) + 1;
-    }
-};
+        return max(up[-1], down[-1])
