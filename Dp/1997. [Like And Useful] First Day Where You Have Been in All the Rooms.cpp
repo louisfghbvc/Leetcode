b967a99ee85958da03d2980f794a5ee0b7ce460c
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-09-05 14:58:54 +0800

    Tricky Dp. Hard.

--- Dp/1997. [Like And Useful] First Day Where You Have Been in All the Rooms.cpp
+++ Dp/1997. [Like And Useful] First Day Where You Have Been in All the Rooms.cpp
@@ -1,4 +1,4 @@
-// Only understand odd even dp.
+// odd even dp.
 // O(N). like graph, transition.
 
 class Solution {
@@ -19,6 +19,28 @@ public:
             long day = dp[i] - dp[nextVisit[i]]; // need odd i - odd nextVisited[i](previous). 
             if(day < mod) day += mod;
             dp2[i] = (dp[i] + 1 + day) % mod;
+        }
+        
+        return dp.back();        
+    }
+};
+
+// ok, so i-1 must be even so that can jump to i.
+// dp[i-1] + 1 - dp[nextVisit[i-1]], because dp is finish, so just simple sub.
+// O(N)
+class Solution {
+public:
+    const int mod = 1e9+7;
+
+    int firstDayBeenInAllRooms(vector<int>& nextVisit) {
+        int n = nextVisit.size();
+        vector<long> dp(n); // dp[i]: finish days
+        
+        for(int i = 1; i < n; ++i){
+            long zoomP = dp[i-1] + 1 - dp[nextVisit[i-1]]; // from zoom nextVisit[i-1] 
+            if(zoomP < mod) zoomP += mod;
+            long zoom0 = dp[i-1] + 1; // from zoom 0
+            dp[i] = (zoom0 + zoomP) % mod;
         }
         
         return dp.back();        
