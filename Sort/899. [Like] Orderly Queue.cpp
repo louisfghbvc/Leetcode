aaffbcd9ad22210ce35d896a44ce15c692051da8
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-09-05 16:22:18 +0800

    Observe pattern.

---
+++ Sort/899. [Like] Orderly Queue.cpp
@@ -0,0 +1,15 @@
+// K > 2 same as bubble sort. tricky.
+class Solution {
+public:
+    string orderlyQueue(string s, int k) {
+        if(k > 1){
+            sort(s.begin(), s.end());
+            return s;
+        }
+        string res = s;
+        for(int i = 1; i < s.size(); ++i){
+            res = min(res, s.substr(i) + s.substr(0, i));
+        }
+        return res;
+    }
+};
