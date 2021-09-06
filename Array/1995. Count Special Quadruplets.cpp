cc56f3858c13a05370ed0ca3d7eebd0ad684a49a
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-09-06 09:45:47 +0800

    BF, Hashtable.

--- Array/1995. Count Special Quadruplets.cpp
+++ Array/1995. Count Special Quadruplets.cpp
@@ -13,3 +13,22 @@ public:
         return cnt;
     }
 };
+
+// Use 3 Hashtable. O(N * X)
+class Solution {
+public:
+    int countQuadruplets(vector<int>& nums) {
+        int n = nums.size();
+        int cnt = 0;
+        unordered_map<int, int> _single, _double, _triple;
+        for(int x: nums){
+            cnt += _triple[x];
+            for(auto &[d, v]: _double) if(d + x <= 100)
+                _triple[d + x] += v;
+            for(auto &[s, v]: _single) if(s + x <= 100)
+                _double[s + x] += v;
+            _single[x]++; 
+        }
+        return cnt;
+    }
+};
