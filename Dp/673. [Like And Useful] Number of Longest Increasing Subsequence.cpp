1502a4c6a598babf33c20b3b1110867f6f7ad8b4
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-09-07 18:00:55 +0800

    O(N^2), LIS + BS + Prefix sum. Patience sort O(NlogN)

--- Dp/673. [Like And Useful] Number of Longest Increasing Subsequence.cpp
+++ Dp/673. [Like And Useful] Number of Longest Increasing Subsequence.cpp
@@ -30,3 +30,48 @@ public:
         return res;
     }
 };
+
+
+// O(NlogN). Patience sorting. 2 binary search + prefix sum. impressive.
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        int n = nums.size();
+        
+        vector<vector<pair<int, int>>> arr(n+1); // like patience sort.
+
+        int max_so_far = 0;
+        for(int x: nums){
+            // search insert position
+            int l = 0, r = max_so_far;
+            while(l < r){
+                int mid = l + (r - l) / 2;
+                if(arr[mid].back().first < x) l = mid+1;
+                else r = mid;
+            }
+            
+            // search numbers of way
+            int ways = 1;
+            int row = l - 1;
+            if(row >= 0){
+                int l1 = 0, r1 = arr[row].size();
+                while(l1 < r1){
+                    int mid = l1 + (r1 - l1) / 2;
+                    if(arr[row][mid].first < x) r1 = mid;
+                    else l1 = mid+1;
+                }
+                
+                ways = arr[row].back().second;
+                // impossible, since bigger than x
+                ways -= (l1 == 0) ? 0 : arr[row][l1 - 1].second;
+            }
+            
+            // insert arr[l] prefix sum.
+            arr[l].push_back({x, arr[l].empty() ? ways: arr[l].back().second + ways});
+            if(l == max_so_far) ++max_so_far;
+        }
+        
+        return arr[max_so_far-1].back().second;
+    }
+};
