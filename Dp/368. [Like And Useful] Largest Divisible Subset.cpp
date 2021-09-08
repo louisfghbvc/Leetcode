1d635272960d3bf795c2da6c1bc5713fd952aa5b
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-09-08 23:11:11 +0800

    LIS

--- Dp/368. [Like And Useful] Largest Divisible Subset.cpp
+++ Dp/368. [Like And Useful] Largest Divisible Subset.cpp
@@ -1,34 +1,39 @@
-// O(N^2)
-// can observe, 1 2 4 8, or 4 8 240.
-// u can see the i, and j(>i). must incresing order mod.
+// O(N^2). LIS. and record path.
 
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        vector<int> lis(n, 1);
-        vector<int> pre(n, -1);
-        int mx = 0, ind = -1;
+        vector<int> res;
+        
+        // dp[i]: longest length, end with i
+        vector<int> dp(n); 
+        // path[i]: longest length, parent
+        vector<int> path(n); 
+        
+        // maximum length index.
+        int mxi = 0;
         for(int i = 0; i < n; ++i){
-            for(int j = i - 1; j >= 0; --j){
-                if(nums[i] % nums[j] == 0){
-                    if(lis[i] < lis[j]+1){
-                        lis[i] = lis[j]+1;
-                        pre[i] = j;
+            dp[i] = 1;
+            path[i] = -1;
+            for(int j = 0; j < i; j++){
+                if(nums[i] % nums[j] == 0){ // can from j
+                    if(dp[i] < dp[j]+1){
+                        dp[i] = dp[j]+1;
+                        path[i] = j;
                     }
                 }
             }
-            if(mx < lis[i]){
-                mx = lis[i];
-                ind = i;
+            if(dp[i] > dp[mxi]){
+                mxi = i;
             }
         }
-        vector<int> res;
-        for(; ind != -1; ind = pre[ind]){
-            res.push_back(nums[ind]);
-        }
         
+        // no order, just push back
+        for(; mxi != -1; mxi = path[mxi]){
+            res.push_back(nums[mxi]);
+        }
         return res;
     }
 };
