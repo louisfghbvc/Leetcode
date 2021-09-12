38ec46bd51d7481cb49fd42abeb4a39b702c3573
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-09-12 22:37:01 +0800

    Tricky. Only consider 1 / Union Find

--- Graph/2003. [Like And Useful] Smallest Missing Genetic Value in Each Subtree.cpp
+++ Graph/2003. [Like And Useful] Smallest Missing Genetic Value in Each Subtree.cpp
@@ -39,3 +39,51 @@ public:
         return ans;
     }
 };
+
+
+// Union Find, Bottom up Dp. ans[u] >= ans[v].
+class Solution {
+public:
+    
+    vector<vector<int>> g; // graph
+    vector<int> num; // genetic value
+    vector<int> p; // union-find
+    
+    vector<int> ans;
+    int n;
+    
+    int find(int x){
+        return p[x] < 0 ? x : p[x] = find(p[x]);
+    }
+    void uni(int x, int y){
+        x = find(x), y = find(y);
+        if(x == y) return;
+        p[y] = x;
+    }
+    
+    int dfs(int u){
+        if(num[u] > n) num[u] = n+1;
+        int res = 1;
+        for(int v: g[u]){
+            res = max(res, dfs(v));
+            uni(num[u], num[v]);
+        }
+        while(res <= n && find(res) == find(num[u])) res++;
+        return ans[u] = res;
+    }
+    
+    vector<int> smallestMissingValueSubtree(vector<int>& parents, vector<int>& nums) {
+        n = parents.size();
+        p.resize(n+2, -1);
+        
+        g.resize(n);
+        for(int i = 1; i < n; ++i)
+            g[parents[i]].push_back(i);
+        num = nums;
+        ans = vector(n, 0);
+        
+        dfs(0);
+        
+        return ans;
+    }
+};
