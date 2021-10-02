14f0ab73008aa19f4eee217f6034055899506b64
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-10-03 01:00:08 +0800

    BF.

---
+++ Array/2022. Convert 1D Array Into 2D Array.cpp
@@ -0,0 +1,14 @@
+// implement. 
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        if(m * n != original.size()) return {};
+        vector<vector<int>> res(m, vector<int>(n));
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+                res[i][j] = original[i * n + j];
+            }
+        }
+        return res;
+    }
+};
