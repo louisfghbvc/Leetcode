c7e2562a62cee7a834d4ff94400d15b8b27e9248
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-10-10 12:57:21 +0800

    Bit manipulation

--- Bit Manipulation/201. [Like]Bitwise AND of Numbers Range.cpp
+++ Bit Manipulation/201. [Like]Bitwise AND of Numbers Range.cpp
@@ -11,3 +11,23 @@ public:
         return m << i;
     }
 };
+
+
+// find msb not zero
+// and if bit not equal break
+// O(1).
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int res;
+        int i;
+        for(i = 30; i >= 0 && (right >> i & 1) == 0; --i);
+        
+        for(; i >= 0; i--){
+            if((left >> i & 1) != (right >> i & 1)) break;
+            if(left & (1<<i))
+                res |= 1<<i;
+        }
+        return res;
+    }
+};
