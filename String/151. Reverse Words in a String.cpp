c33e569eba04b849537c3f1687c957c486d05fe3
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-10-20 10:57:15 +0800

    Inplace

--- String/151. Reverse Words in a String.cpp
+++ String/151. Reverse Words in a String.cpp
@@ -17,3 +17,59 @@ public:
         return res;
     }
 };
+
+
+// O(N) Inplace
+// reverse whole string
+// reverse each string
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        reverse(s.begin(), s.end());
+        
+        int storeIndex = 0;
+        for(int i = 0; i < s.size(); ++i){
+            if(s[i] == ' ') continue;
+            if(storeIndex != 0) s[storeIndex++] = ' ';
+            int j = i;
+            while(j < s.size() && s[j] != ' ') s[storeIndex++] = s[j++];
+            reverse(s.begin() + storeIndex - (j - i), s.begin() + storeIndex);
+            i = j;
+            
+        }
+        
+        s.erase(s.begin() + storeIndex, s.end());
+        
+        return s;
+    }
+};
+
+// original 
+class Solution {
+public:
+    string reverseWords(string s) {
+        reverse(s.begin(), s.end());
+        
+        for(int i = 0, j = 0; i < s.size(); i = j){
+            while(i < s.size() && s[i] == ' ') i++;
+            j = i;
+            while(j < s.size() && s[j] != ' ') j++;
+            reverse(s.begin()+i, s.begin()+j);
+        }
+        
+        int index_fillin = 0;
+        for(int i = 0; i < s.size(); ++i){
+            if(s[i] == ' ') continue;
+            else{
+                while(i < s.size() && s[i] != ' ') s[index_fillin++] = s[i++];
+                if(index_fillin < s.size())
+                    s[index_fillin++] = ' ';
+            }
+        }
+        
+        while(s.size() > index_fillin || s.back() == ' ') s.pop_back();
+        
+        return s;
+    }
+};
