acb8b176e109a37d4d4dff18cc5b9383f29d8898
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-10-27 08:49:55 +0800

    Two pointers. counting sort

--- Two Pointers/75. Sort Colors.cpp
+++ Two Pointers/75. Sort Colors.cpp
@@ -10,3 +10,43 @@ public:
         }
     }
 };
+
+
+// Counting sort O(N).
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int cnt[3] = {};
+        for(int x: nums) cnt[x]++;
+        
+        int i = 0;
+        for(int k = 0; k < 3; ++k){
+            while(cnt[k]--) nums[i++] = k;
+        }
+        
+    }
+};
+
+// One pass. straight forward.
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        
+        int zero = 0, two = n - 1;
+        for(int i = 0; i <= two; ){
+            if(nums[i] == 0){
+                swap(nums[zero], nums[i]);
+                zero++;
+                i++;
+            }
+            else if(nums[i] == 2){
+                swap(nums[two], nums[i]);
+                two--;
+            }
+            else i++;
+        }
+        
+    }
+};
