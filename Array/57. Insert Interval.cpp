78c899cb816aa5c261be8e5501f69ba99a50d663
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-10-30 14:44:19 +0800

    merge interval.

--- Array/57. Insert Interval.cpp
+++ Array/57. Insert Interval.cpp
@@ -24,3 +24,31 @@ public:
         return res;
     }
 };
+
+
+// simple clear.
+// O(N)
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> res;
+        int n = intervals.size();
+                
+        for(auto &in: intervals){
+            if(newInterval[1] < in[0]){
+                res.push_back(newInterval);
+                newInterval = in;
+            }
+            else if(in[1] < newInterval[0]){
+                res.push_back(in);
+            }
+            else{
+                newInterval[0] = min(in[0], newInterval[0]);
+                newInterval[1] = max(in[1], newInterval[1]);
+            }
+        }
+        res.push_back(newInterval);
+        
+        return res;
+    }
+};
