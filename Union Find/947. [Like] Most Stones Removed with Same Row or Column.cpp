bcc9702d3db3134c3dee09940f2b318b1312626c
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-11-03 10:54:15 +0800

    Union find, trick

--- Union Find/947. [Like] Most Stones Removed with Same Row or Column.cpp
+++ Union Find/947. [Like] Most Stones Removed with Same Row or Column.cpp
@@ -37,3 +37,52 @@ public:
         return n - group;
     }
 };
+
+
+// find connected components
+// O(N).
+class Solution {
+public:
+    
+    vector<int> p;
+    int find(int x){
+        return p[x] < 0 ? x : p[x] = find(p[x]);
+    }
+    void uni(int x, int y){
+        x = find(x), y = find(y);
+        if(y == x) return;
+        if(p[x] > p[y]) swap(x, y);
+        p[x] += p[y];
+        p[y] = x;
+    }
+    
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        p = vector<int>(n, -1);
+        
+        // union find to union connected components
+        unordered_map<int, int> cols, rows;
+        for(int i = 0; i < n; ++i){
+            if(cols.count(stones[i][1])){
+                uni(i, cols[stones[i][1]]);
+            }
+            else{
+                cols[stones[i][1]] = i;
+            }
+            if(rows.count(stones[i][0])){
+                uni(i, rows[stones[i][0]]);
+            }
+            else{
+                rows[stones[i][0]] = i;
+            }
+        }
+        
+        // ans is sum of each componect group size - 1
+        int res = 0;
+        for(int i = 0; i < n; ++i){
+            if(p[i] < 0) res += abs(p[i]) - 1;
+        }
+        
+        return res;
+    }
+};
