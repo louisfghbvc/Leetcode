7f853f6ec41d35511919353e531b55ce21dc7a01
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-11-12 15:38:50 +0800

    recursive, simple

--- Linked List/203. Remove Linked List Elements.cpp
+++ Linked List/203. Remove Linked List Elements.cpp
@@ -26,3 +26,25 @@ public:
         return head;
     }
 };
+
+
+// recursive
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(!head) return nullptr;
+        if(head->val == val) return removeElements(head->next, val);
+        head->next = removeElements(head->next, val);
+        return head;
+    }
+};
