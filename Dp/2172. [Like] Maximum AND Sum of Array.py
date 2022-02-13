83602acde890f34f04682392c25ae44c47f4f9ad
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2022-02-13 14:41:48 +0800

    Bitmask, backtracking

--- Dp/2172. [Like] Maximum AND Sum of Array.py
+++ Dp/2172. [Like] Maximum AND Sum of Array.py
@@ -1,3 +1,6 @@
+# O(N^2*4^N), n = numsSlot
+# use 2 bit
+
 class Solution:
     def maximumANDSum(self, nums: List[int], numSlots: int) -> int:
         # backtracking
@@ -19,3 +22,26 @@ class Solution:
             return res
         
         return dfs(0)
+
+# use 3-base
+class Solution:
+    def maximumANDSum(self, nums: List[int], numSlots: int) -> int:
+        # backtracking
+        # try to enumerate all possible
+        
+        n = len(nums)
+        
+        @lru_cache(None)
+        def dfs(i, mask):
+            if i == n: return 0
+            
+            res = 0
+            bit = 1
+            for slot in range(numSlots):
+                if mask // bit % 3 > 0:
+                    res = max(res, (nums[i]&(slot+1)) + dfs(i+1, mask - bit))
+                bit *= 3
+            
+            return res
+        
+        return dfs(0, 3 ** numSlots - 1)
