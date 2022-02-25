6655028947b88789316c7baa05ac60494c40e27b
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2022-02-25 16:28:34 +0800

    stringstream

--- String/165. Compare Version Numbers.cpp
+++ String/165. Compare Version Numbers.cpp
@@ -42,3 +42,67 @@ public:
         return com(v1, v2);
     }
 };
+
+
+// use custom string iter
+class Iter{
+public:
+    Iter(string &s): s(s), i(0) {}
+    bool hasNext(){
+        return i < s.size();    
+    }
+    int get(){
+        int tmp = 0;
+        while(i < s.size() && s[i] != '.')
+            tmp = tmp * 10 + s[i++] - '0';
+        i++;
+        return tmp;
+    }
+    
+private:
+    string s;
+    int i;
+};
+
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        // revisions constraint
+        
+        Iter a(version1), b(version2);
+        
+        while(a.hasNext() || b.hasNext()){
+            int va = 0, vb = 0;
+            if(a.hasNext())
+                va = a.get();
+            if(b.hasNext())
+                vb = b.get();
+            if(va != vb)
+                return (va < vb) ? -1 : 1;
+        }
+        
+        return 0;
+    }
+};
+
+// use stringstream, concise
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        // use stringstream
+
+        for(auto &c: version1) if(c == '.') c = ' ';
+        for(auto &c: version2) if(c == '.') c = ' ';
+        
+        istringstream s1(version1), s2(version2);
+        while(1){
+            int number1, number2;
+            if(!(s1 >> number1)) number1 = 0;
+            if(!(s2 >> number2)) number2 = 0;
+            if(!s1 && !s2) return 0;
+            if(number1 < number2) return -1;
+            if(number1 > number2) return 1;
+        }
+        return 0;
+    }
+};
