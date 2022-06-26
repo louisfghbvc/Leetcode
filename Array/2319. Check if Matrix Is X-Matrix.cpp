cb79994cc0907cfe8dad19f6a9c5b9675582840a
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2022-06-26 14:44:58 +0800

    matrix

---
+++ Array/2319. Check if Matrix Is X-Matrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkXMatrix(vector<vector<int>>& grid) {
+        
+        int n = grid.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == j || i == n-j-1) { // diagoal
+                    if (grid[i][j] == 0) return false;
+                }
+                else {
+                    if (grid[i][j]) return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+};
