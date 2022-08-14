9aca8e5d2be3a09eb14d5b6b66d23317e3ac8af9
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2022-08-14 14:55:39 +0800

    digit dp

---
+++ Dp/2376. [Like] Count Special Integers.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int countSpecialNumbers(int n) {
+        // special: all the digits are distinct
+        // goal: calculate the [1, n] how many number of special value
+        
+        // idea: digit dp
+        // TC: O(d*2^d*d) SC: O(d*2^d*d)
+        
+        
+        string s = to_string(n);
+        
+        int dp[10][1<<10][2][2];
+        memset(dp, -1, sizeof dp);
+        
+        
+        auto dfs = [&](const auto &self, int i, int vis, bool isbound=true, bool leading_zero=true) -> int {
+            if (i == s.size()) return !leading_zero;  // base case, 0
+            
+            int &res = dp[i][vis][isbound][leading_zero];
+            if (res != -1) return res; // memo
+            
+            res = 0;
+            int bd = isbound ? s[i]-'0' : 9;
+            for (int d = 0; d <= bd; ++d) {
+                bool nbound = isbound && d == s[i]-'0';
+                bool nlz = leading_zero && d == 0;
+                int nmask = nlz ? 0 : vis|(1<<d);
+                if (vis >> d & 1) continue; // same digit
+                res += self(self, i+1, nmask, nbound, nlz);
+            }
+            
+            return res;
+        };
+        
+        return dfs(dfs, 0, 0);
+    }
+};
