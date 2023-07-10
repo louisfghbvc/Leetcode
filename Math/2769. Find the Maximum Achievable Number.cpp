f8bee6036c17f4764db5a790f7ec41a5420426b7
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2023-07-10 21:43:47 +0800

    Create 2769. Find the Maximum Achievable Number.cpp

---
+++ Math/2769. Find the Maximum Achievable Number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+        // goal: find the maximum x
+        
+        // num is even
+        // num + 2*t
+        
+        // num is odd
+        
+        // 3, 1
+        
+        // 3 -> 5
+        
+        
+        return num + 2*t;
+    }
+};
