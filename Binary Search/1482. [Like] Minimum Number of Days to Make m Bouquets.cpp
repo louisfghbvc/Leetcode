71b091a7aa5445b745e97224d1aab010cbd99843
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2024-06-19 13:30:34 +0800

    Binary Search the ans

--- Binary Search/1482. [Like] Minimum Number of Days to Make m Bouquets.cpp
+++ Binary Search/1482. [Like] Minimum Number of Days to Make m Bouquets.cpp
@@ -1,40 +1,45 @@
 // Hard to think. Binary Search. 
-// The magic is guess ans, and divide and conquer
+// The magic is guess ans, then using two pointers to compute the bouquet
 // O(NlogV)
 
 class Solution {
 public:
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int n = bloomDay.size();
-        if(n/k < m) return -1;
-        vector<int> t = bloomDay;
-        sort(t.begin(), t.end());
-        vector<bool> vis(n);
-        int l = *t.begin(), r = *t.rbegin();
-        int res = 0;
-        while(l <= r){
+        // bloomDay, m, k
+        // make a bouquet need k flowers from the garden
+        // each flower will used in exactly one bouquet
+        // bloomDay[i]: flower will bloom after this day
+
+        // idea:
+        // impossible: k*m > n
+        // [1,10,3,10,2], m = 3, k = 2 => impossible
+        // binary search the days
+        // F(d) -> numbers of bloomdays we can make
+        // when given a days, we can use two pointers to calculate the bouquet
+        // O(nlogX)
+        
+        auto good = [&](int day) -> int {
+            int res = 0;
+            for (int i = 0, j; i < bloomDay.size(); i = j) {
+                j = i;
+                while (j < bloomDay.size() && bloomDay[j] <= day) j++;
+                int len = j-i;
+                res += len / k;
+                if (j == i) j++; // no flowers on this day
+            }
+            return res;
+        };
+
+        int l = 0, r = 1e9;
+        int ans = -1;
+        while (l <= r) {
             int mid = (l+r)/2;
-            for(int i = 0; i < n; ++i){
-                if(bloomDay[i] <= mid) vis[i] = 1;
-                else vis[i] = 0;
-            }
-            int c = 0, tc = 0;
-            for(int i = 0; i < n; ++i){
-                if(vis[i]) c++;l
-                else c= 0;
-                if(c == k){
-                    tc++;
-                    c = 0;
-                }
-            }
-            if(tc >= m){
-                res = mid;
-                r = mid-1;
-            }
-            else{
-                l = mid+1;
-            }
+            if (good(mid) >= m) ans = mid, r = mid-1;
+            else l = mid+1;
         }
-        return res;
+
+        return ans;
     }
 };
+
+
