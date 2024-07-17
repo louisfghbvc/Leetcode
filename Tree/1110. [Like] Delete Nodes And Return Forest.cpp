5a83b1ead38c0e038c06015f7a8aaaad873e8c9f
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2024-07-17 08:25:14 +0800

    Recursive

--- Tree/1110. [Like] Delete Nodes And Return Forest.cpp
+++ Tree/1110. [Like] Delete Nodes And Return Forest.cpp
@@ -35,3 +35,49 @@ private:
     unordered_set<int> to_del;
     vector<TreeNode*> res;
 };
+
+
+// Not neat, still can improve to version1 above
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> ans;
+    TreeNode* dfs(TreeNode* root, unordered_set<int>& to_delete) {
+        if (!root) return nullptr;
+        root->left = dfs(root->left, to_delete);
+        root->right = dfs(root->right, to_delete);
+        if (to_delete.count(root->val)) {
+            if (root->left) {
+                ans.push_back(root->left);
+            }
+            if (root->right) {
+                ans.push_back(root->right);
+            }
+            return nullptr;
+        }
+        return root;
+    }
+
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        // goal: when we remove a node, the left and right become a root
+        // idea:
+        // traverse the tree, when meets to_delete -> add left and right
+        // how to delete the node in tree?
+        // we can make it nullptr
+        unordered_set<int> st(to_delete.begin(), to_delete.end());
+        dfs(root, st);
+        if (!st.count(root->val))
+            ans.push_back(root);
+        return ans;
+    }
+};
