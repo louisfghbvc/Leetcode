167fab27475935849a2429c4a204680e433399cd
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2024-08-11 08:56:14 +0800

    Greedy, at most 2

--- Greedy/1568. [Like And Useful] Minimum Number of Days to Disconnect Island.cpp
+++ Greedy/1568. [Like And Useful] Minimum Number of Days to Disconnect Island.cpp
@@ -1,52 +1,56 @@
-// 0 or 1 or 2
-// at most 2. think about if a edge cell connect to land at most 2 bridge right? QQ
-// thats why greedy...
-// O(N^4)
-
 class Solution {
 public:
-    vector<vector<int>> g;
-    int n, m;
-    const int dir[4][2] = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-    
-    void dfs(int x, int y){
-        g[x][y] = 0;
-        for(int k = 0; k < 4; ++k){
-            int nx = x+dir[k][0], ny = y+dir[k][1];
-            if(nx < 0 || ny < 0 || nx >= n || ny >= m || !g[nx][ny]) continue;
-            dfs(nx, ny);
-        }
-    }
-    
-    int cntIsland(){
+
+    int computeIsland(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+
+        vector vis(m, vector(n, false));
+        auto dfs = [&](auto &self, int i, int j) -> void {
+            for (auto &[ni, nj]: {make_pair(i+1,j),{i-1,j},{i,j-1},{i,j+1}}) {
+                if (ni >= m || ni < 0 || nj >= n || nj < 0 || vis[ni][nj] || !grid[ni][nj]) continue;
+                vis[ni][nj] = 1;
+                self(self, ni, nj);
+            }
+        };  
+
         int cnt = 0;
-        for(int i = 0; i < n; ++i){
-            for(int j = 0; j < m; ++j){
-                if(g[i][j]){
-                    cnt++;
-                    dfs(i, j);
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j]) {
+                    if (vis[i][j] == 0) {
+                        vis[i][j] = 1;
+                        cnt++;
+                        dfs(dfs, i, j);
+                    }
                 }
             }
         }
+
         return cnt;
     }
-    
+
     int minDays(vector<vector<int>>& grid) {
-        g = grid;
-        n = grid.size(), m = grid[0].size();
-        if(cntIsland() > 1) return 0;
-        
-        for(int i = 0; i < n; ++i){
-            for(int j = 0; j < m; ++j){
-                if(grid[i][j]){
-                    grid[i][j] = 0;
-                    g = grid;
-                    if(cntIsland() > 1) return 1;
-                    grid[i][j] = 1;
+        // goal: find the minimum day to split grid to two components
+        // idea:
+        // ans is at most 2
+
+        // ans is 0, if island > 1
+        // ans is 1 ?
+        // enumerate all cells if 1, remove it
+
+        if (computeIsland(grid) > 1 || computeIsland(grid) == 0) return 0;
+
+        int m = grid.size(), n = grid[0].size();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j]) {
+                    grid[i][j] ^= 1;
+                    if (computeIsland(grid) > 1 || computeIsland(grid) == 0) return 1;
+                    grid[i][j] ^= 1;
                 }
             }
         }
-        
+
         return 2;
     }
 };
