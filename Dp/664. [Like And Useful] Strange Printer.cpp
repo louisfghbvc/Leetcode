b82e10a2899868b753960ed0752071111b0092c0
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2024-08-21 10:53:28 +0800

    Interval dp

--- Dp/664. [Like And Useful] Strange Printer.cpp
+++ Dp/664. [Like And Useful] Strange Printer.cpp
@@ -1,42 +1,60 @@
-// O(N^3), interval dp
-
 class Solution {
 public:
-    
-    // dp[l][r] -> min(dp[l][k] + dp[k+1][r])... j = l~r-1;
-    // base case: [l, r] == 1 -> 1
-    // [l, r] == 2 -> 1 or 2
-    
-    //    aba
-    //    /  \
-    // ab,a a,ba
-    // don't need to paint r when, [l, k], [k+1, r], s[k] == s[r]
-    
-    
-    vector<vector<int>> dp;
-    
-    int dfs(int l, int r){
-        if(l == r) return 1;
-        else if(r-l == 1) return s[l] == s[r] ? 1 : 2;
-        
-        // for memo
-        int &res = dp[l][r];
-        if(res != -1) return res;
-        
-        res = 1e9;
-        for(int k = l; k < r; ++k){
-            res = min(dfs(l, k) + dfs(k+1, r) - (s[k] == s[r]), res);
-        }
-        
-        return res;
-    }
-    
     int strangePrinter(string s) {
-        this->s = s;
-        dp = vector<vector<int>>(s.size(), vector<int>(s.size(), -1));
-        return dfs(0, s.size()-1);
+        // goal: find out the minimum number of print to make to string
+        // print we can choose [l, r] -> print a letter
+
+        // idea:
+        // e.g
+        // aba => aaa
+        //         b
+        // a a a a
+        //   b b
+        //   c
+
+        //.       l
+        //.         r
+        // [a b a c a]
+        //  ^   ^   ^
+        //  a b c
+
+        // a b c a b s b
+        // ^     ^
+
+        // a b c a b c a
+        // 
+
+        // dp
+        // dfs(i, j): minimum turn to make s[i:j] paint
+        // we always need paint first letter s[i]
+        // each [i, j] we use two pointers to record window that is not s[i]
+
+        // after looking the solution
+        // we can just consider s[l] == s[r]
+        // if s[l] == s[r] => dfs(l, r-1), since r can always be done with no cost
+        // else
+        // enumerate the middle 
+
+        int n = s.size();
+        vector dp(n, vector(n, -1));
+        auto dfs = [&](auto &self, int l, int r) -> int {
+            if (l > r) return 0;
+            if (l == r) return 1;
+
+            int &res = dp[l][r];
+            if (res != -1) return res;
+
+            res = n;
+            if (s[l] == s[r])
+                res = self(self, l, r-1);
+            else {
+                for (int k = l; k < r; ++k)
+                    res = min(res, self(self, l, k) + self(self, k+1, r));
+            }
+
+            return res;
+        };
+
+        return dfs(dfs, 0, n-1);
     }
-    
-private:
-    string s;
 };
