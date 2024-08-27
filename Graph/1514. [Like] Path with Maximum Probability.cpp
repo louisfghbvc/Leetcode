a226081a850243d7f7abdb7052fb5d82f7f014c3
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2024-08-27 12:06:17 +0800

    dijkstra

--- Graph/1514. [Like] Path with Maximum Probability.cpp
+++ Graph/1514. [Like] Path with Maximum Probability.cpp
@@ -2,32 +2,34 @@
 // Can also use BFS. because no need to sort. BFS more faster
 
 class Solution {
-typedef pair<double, int> di;
 public:
-    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        vector<di> g[n];
-        for(int i = 0; i < edges.size(); ++i){
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
+        // goal: find the maximum probability
+        // idea:
+        // build graph + dijkstra
+
+        vector<vector<pair<int, double>>> graph(n);
+        for (int i = 0; i < edges.size(); ++i) {
             int a = edges[i][0], b = edges[i][1];
-            double w = succProb[i];
-            g[a].push_back({w, b});
-            g[b].push_back({w, a});
+            graph[a].emplace_back(b, succProb[i]);
+            graph[b].emplace_back(a, succProb[i]);
         }
-        
-        priority_queue<di> pq;
-        vector<double> d(n, 0.0);
-        d[start] = 1.0;
-        pq.push({1.0, start});
-        while(pq.size()){
-            di tmp = pq.top(); pq.pop();
-            int u = tmp.second; double c = tmp.first;
-            for(auto &nt: g[u]){
-                int v = nt.second; double w = nt.first;
-                if(d[u]*w > d[v]){
-                    d[v] = d[u]*w;
-                    pq.push({d[u]*w, v});
+
+        vector<double> dp(n, 0);
+        dp[start_node] = 1;
+        priority_queue<pair<double, int>> pq;
+        pq.push({1, start_node});
+        while (pq.size()) {
+            auto [cost, u] = pq.top(); pq.pop();
+            if (dp[u] != cost) continue;
+            for (auto &[v, w]: graph[u]) {
+                if (dp[v] == 0 || cost * w > dp[v]) {
+                    dp[v] = cost*w;
+                    pq.push({cost*w, v});
                 }
             }
         }
-        return d[end];
+
+        return dp[end_node];
     }
 };
