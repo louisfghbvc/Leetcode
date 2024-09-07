4bb10fd944cd3bae560f1ec06c5d95b852aa2312
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2024-09-07 18:16:41 +0800

    dfs, kmp

--- Linked List/1367. [Like]Linked List in Binary Tree.cpp
+++ Linked List/1367. [Like]Linked List in Binary Tree.cpp
@@ -38,3 +38,72 @@ public:
         return all(head, root);
     }
 };
+
+// KMP, convert linked list to array and do kmp to efficient compare
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+   vector<int> buildTable(vector<int> &arr) {
+        int n = arr.size();
+        vector<int> res(n);
+        for (int i = 1, j = 0; i < n; ++i) {
+            while (j && arr[i] != arr[j]) j = res[j-1];
+            if (arr[i] == arr[j]) res[i] = ++j;
+        }
+        return res;
+    }
+
+    bool isSubPath(ListNode* head, TreeNode* root) {
+        // goal: find out is head is a sub path of a tree
+        // idea:
+        // we can start from each node
+        // try to compare the list node
+        // O(n*l)
+        // follow up: how to optimized?
+        // it's like string compare
+        // we can trying to do kmp search in the tree
+        // tree => as sequence
+
+        vector<int> arr;
+        while (head) {
+            arr.push_back(head->val);
+            head = head->next;
+        }
+        auto lps = buildTable(arr);
+
+        auto dfs = [&](auto &self, int i, TreeNode* root) -> bool {
+            if (i >= arr.size()) return true;
+            if (!root) return false;
+            
+            // move
+            while (i && arr[i] != root->val)
+                i = lps[i-1];
+
+            if (arr[i] == root->val) ++i;
+            return self(self, i, root->left) || self(self, i, root->right);
+        };  
+
+        return dfs(dfs, 0, root);
+    }
+};
