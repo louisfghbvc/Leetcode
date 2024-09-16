e39e075365d695d25358a9c5c686c9bef327bad7
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2024-09-16 08:58:52 +0800

    Sort

--- Math/539. [Like] Minimum Time Difference.cpp
+++ Math/539. [Like] Minimum Time Difference.cpp
@@ -31,3 +31,46 @@ public:
         return res;
     }
 };
+
+
+// Sort
+
+class Solution {
+public:
+    int conv(string& s) {
+        int h = (s[0]-'0')*10 + (s[1]-'0');
+        int m = (s[3]-'0')*10 + (s[4]-'0');
+        return h * 60 + m;
+    }
+
+    int findMinDifference(vector<string>& timePoints) {
+        // goal: find the minimum diff in two list
+
+        // e.g
+        // ["23:59", "00:00"] => dif = 1
+        // means => 0 = 24:00
+
+        // idea:
+        // we convert time to minutes
+        // we sort time points, add padding 24:00
+
+        // ["23:59", "00:00"] => [0:00, 23:59, 24:00]
+
+        vector<int> arr;
+        for (auto &s: timePoints)
+            arr.push_back(conv(s));
+
+        ranges::sort(arr);
+
+        int n = arr.size();
+        arr.resize(2*n);
+        for (int i = 0; i < n; ++i)
+            arr[n+i] = arr[i] + 24*60;
+
+        int ans = 1e9;
+        for (int i = 1; i < 2*n; ++i)
+            ans = min(ans, arr[i] - arr[i-1]);
+
+        return ans;
+    }
+};
