c8c106ffc148b38cfef80917bbb5167f06828ff0
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2024-09-18 11:02:42 +0800

    Sort

--- Sort/179. Largest Number.cpp
+++ Sort/179. Largest Number.cpp
@@ -4,15 +4,36 @@
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end(), [&](int a, int b){
-            string ta = to_string(a), tb = to_string(b);
-            return ta+tb > tb+ta ? true : false;
+        // goal: find out the largest number 
+        // we can rearrange them
+
+        // idea:
+        // "12" "3"
+
+        // consider
+        // "12" "121"
+        // "12121" vs "12112"
+
+        // sort by concate them, and consider which one is largest
+
+        // e.g "9" "8" "7"
+        // if 9 > 8, means 9 > 7
+
+        sort(nums.begin(), nums.end(), [](auto &a, auto &b){
+            string left = to_string(a) + to_string(b);
+            string right = to_string(b) + to_string(a);
+            return left > right;
         });
-        string res = "";
-        for(int x: nums){
-            if(res.empty() && x == 0) continue; 
+
+        string res;
+        for (auto &x: nums)
             res += to_string(x);
-        }
-        return res.empty() ? "0" : res;
+
+        // need to fix leading zero!!
+        reverse(res.begin(), res.end());
+        while (res.size() > 1 && res.back() == '0') res.pop_back();
+        reverse(res.begin(), res.end());
+
+        return res;
     }
 };
