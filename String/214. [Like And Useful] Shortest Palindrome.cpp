bf738d45a8c1e33f14f1e88a97c17204f8b60f41
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2024-09-20 18:10:09 +0800

    KMP, Z, Hash

--- String/214. [Like And Useful] Shortest Palindrome.cpp
+++ String/214. [Like And Useful] Shortest Palindrome.cpp
@@ -72,3 +72,44 @@ public:
     }
 };
 
+
+// Rolling hash!
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        // goal: find out the shortest palindrome to add chars in front of it
+        // idea:
+        // e.g
+        // abc => cbabc
+
+        // we just add reverse suffix into prefix
+        // s = t + suf
+        // rev(suf) + t + suf
+
+        // we just make sure the prefix is palindrome!
+        // we want to find out the longest prefix that is palindrome
+
+        // e.g
+        // (aa)bc
+
+        // we can using rolling hash to do this
+        // aba
+        // x10^2 + 10^1y + z 
+        // x + 10^1y + 10^2z ....
+        
+        unsigned long left = 0, powB = 1, right = 0;
+
+        int n = s.size();
+        int idx = -1;
+        for (int i = 0; i < n; ++i) {
+            left = left * 37 + s[i]-'a';
+            right += powB * (s[i]-'a');
+            if (left == right) idx = i;
+            powB *= 37;
+        }
+
+        string suf = s.substr(idx+1);
+        reverse(suf.begin(), suf.end());
+        return suf + s;
+    }
+};
