d5212b7597c3bee96987e86fd2aca5750d45462a
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2024-09-30 12:43:47 +0800

    Lazy propagation

--- Design/1381. Design a Stack With Increment Operation.cpp
+++ Design/1381. Design a Stack With Increment Operation.cpp
@@ -34,3 +34,55 @@ public:
  * int param_2 = obj->pop();
  * obj->increment(k,val);
  */
+
+
+// lazy update, O(1)
+// we only mark the last index.
+
+class CustomStack {
+public:
+
+    // idea:
+    // how to improve the range update?
+    // we can just using the index for lazy propagate.
+
+    // size = 4
+    // k = 3
+    // [a b c d]
+    //  0 0 x
+
+    CustomStack(int maxSize) : maxSize(maxSize) {
+
+    }
+    
+    void push(int x) {
+        if (st.size() == maxSize) return;
+        st.push_back(x);
+        inc.push_back(0);
+    }
+    
+    int pop() {
+        if (st.empty()) return -1;
+
+        int lastIndex = st.size()-1;
+        int ret = st.back() + inc[lastIndex]; 
+        st.pop_back();
+
+        // lazy update
+        if (lastIndex > 0)
+            inc[lastIndex-1] += inc[lastIndex];
+        inc.pop_back();
+        
+        return ret;
+    }
+    
+    void increment(int k, int val) {
+        int i = min((int)inc.size(), k);
+        if (i > 0)
+            inc[i-1] += val;
+    }
+
+private:
+    vector<int> st, inc;
+    int maxSize;
+};
