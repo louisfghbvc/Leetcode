a68649975c6c14af36cd965f2e423bf1dcfac088
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2024-10-04 12:55:57 +0800

    Two sum

--- Greedy/2491. Divide Players Into Teams of Equal Skill.cpp
+++ Greedy/2491. Divide Players Into Teams of Equal Skill.cpp
@@ -25,3 +25,40 @@ public:
         return ans;
     }
 };
+
+
+// O(n), two sum
+class Solution {
+public:
+    long long dividePlayers(vector<int>& skill) {
+        // goal: find out a divide ways such that all team sum is equal
+        // find the sum of product of a team
+
+        // idea:
+        // think about n is even
+        // sum / (n/2)
+        // [1,2,3,4] => the sum is fix     
+        // we can just using two sum! to find the proper value!
+
+        int n = skill.size();
+        long sum = accumulate(skill.begin(), skill.end(), 0LL);
+        if (sum % (n/2)) return -1;
+        sum /= n/2;
+
+        unordered_map<int, int> idx;
+        long ans = 0;
+        int cnt = 0;
+        for (int x: skill) {
+            if (idx[sum-x] > 0) {
+                idx[sum-x]--;
+                ans += x * (sum-x);
+                cnt++; // find a pair
+            }
+            else {
+                idx[x]++;
+            }
+        }
+
+        return cnt == n/2 ? ans : -1;
+    }
+};
