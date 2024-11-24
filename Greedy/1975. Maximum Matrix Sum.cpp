fe0636889d102dd93a90f390ade0b2fd1fc2a85e
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2024-11-24 19:07:50 +0800

    Greedy

--- Greedy/1975. Maximum Matrix Sum.cpp
+++ Greedy/1975. Maximum Matrix Sum.cpp
@@ -1,19 +1,48 @@
-// Just count neg count, and two case.
 class Solution {
 public:
     long long maxMatrixSum(vector<vector<int>>& matrix) {
+        // goal: maximum the sum of matrix such that we can choose adjacent elements
+        // multiply each of them by -1
+
+        // idea:
+        // [-x  -x]
+        // [x  x] 
+
+        // [-x] y 
+        // [-x] y
+
+        // think the dp
+        // dp[i][j][1]: ending at (i,j) maximum we can get, flip or not
+        // consider (i,j) flip or not
+        // dp not work
+
+        // idea2: looks constructive
+        // any two negative one can always erase it.
+        // when we flip we can make negative move to any position
+        // [-1] [1]
+        // [ 1] [-1] => flip both -1 
+        //  1 1 1
+        //  1 -1 1
+        //  1 1 -1 
+
         int n = matrix.size();
-        
-        int num = 0;
+        int neg = 0;
+        int mn = INT_MAX;
         long sum = 0;
-        int mn = abs(matrix[0][0]);
-        for(int i = 0; i < n; ++i){
-            for(int j = 0; j < n; ++j){
-                num += matrix[i][j] < 0; 
-                sum += abs(matrix[i][j]);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                neg += matrix[i][j] < 0;
                 mn = min(mn, abs(matrix[i][j]));
+                sum += abs(matrix[i][j]);
             }
         }
-        return num & 1 ? sum - 2*mn : sum;
+        
+        // odd
+        if (neg % 2) {
+            return sum - 2*mn;
+        }
+        else {
+            return sum;
+        }
     }
 };
